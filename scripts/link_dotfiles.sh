36a569b97acbe38fe93759d433c040bab75e1f08
Jeff Parsons <jeff.parsons@donuts.co>
2016-06-09 14:33:35 -0700

    added hammerspoon config

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -2,3 +2,4 @@ ln -s ~/.dotfiles/zshrc/zshrc-common ~/.zshrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
