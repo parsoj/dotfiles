3c60ad197f20d8b9516b2f3019d1ad6481bbd29c
Jeff Parsons <jeff.parsons@donuts.co>
2016-07-06 09:35:52 -0700

    remove prexisting dotfiles in linking script

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -1,6 +1,6 @@
-ln -s ~/.dotfiles/zshrc/zshrc-common ~/.zshrc
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/spacemacs ~/.emacs.d
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
+rm ~/.zshrc && ln -s ~/.dotfiles/zshrc/zshrc-common ~/.zshrc
+rm -rf ~/.vim && ln -s ~/.dotfiles/vim ~/.vim
+rm ~/.vimrc && ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+rm -rf ~/.emacs.d && ln -s ~/.dotfiles/spacemacs ~/.emacs.d
+rm ~/.gitconfig && ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+rm ~/.hammerspoon && ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
