1853d1b0ac040fc0b690394bce03a57fbab2938d
Jeff Parsons <jeff.parsons@donuts.co>
2016-07-06 18:31:06 -0700

    spacemacs back as submodule, link script fix

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -3,4 +3,4 @@ rm -rf ~/.vim && ln -s ~/.dotfiles/vim ~/.vim
 rm ~/.vimrc && ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 rm -rf ~/.emacs.d && ln -s ~/.dotfiles/spacemacs ~/.emacs.d
 rm ~/.gitconfig && ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-rm ~/.hammerspoon && ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
+rm -rf ~/.hammerspoon && ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
