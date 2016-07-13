a6372f00b0008a00691d4994996f0119371139f4
Jeff Parsons <jeff.parsons@donuts.co>
2016-07-12 21:36:09 -0700

    fix linking script

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -1,7 +1,7 @@
 rm ~/.zshrc && ln -s ~/.dotfiles/zshrc/zshrc-common ~/.zshrc
 rm -rf ~/.vim && ln -s ~/.dotfiles/vim ~/.vim
 rm ~/.vimrc && ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-rm -rf ~/.emacs.d && ln -s ~/.dotfiles/emacs/spacemacs ~/.emacs.d
-rm -rf ~/.emacs.d/init.el && ln -s ~/.dotfiles/emacs/init.el ~/.emacs.d/
+rm -rf ~/.emacs.d && mkdir ~/.emacs.d && cp -r ~/.dotfiles/spacemacs/* ~/.emacs.d/
+rm ~/.emacs.d/init.el && ln -s ~/.dotfiles/emacs/init.el ~/.emacs.d/
 rm ~/.gitconfig && ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm -rf ~/.hammerspoon && ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
