978c6c6a9687830316d8898225d3b00f097b3f90
Jeff Parsons <jeff.parsons@donuts.co>
2016-08-17 14:03:37 -0700

    updated spacemacs dir linker

--- setup/linking/common.sh
+++ setup/linking/common.sh
@@ -1,6 +1,6 @@
 rm -rf ~/.vim ; ln -s ~/.dotfiles/vim ~/.vim
 rm ~/.vimrc ; ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-rm -rf ~/.emacs.d ; mkdir ~/.emacs.d && cp -r ~/.dotfiles/spacemacs/* ~/.emacs.d/
+rm -rf ~/.emacs.d ; mkdir ~/.emacs.d && cp -r ~/.dotfiles/emacs/spacemacs/* ~/.emacs.d/
 rm ~/.emacs.d/init.el ; ln -s ~/.dotfiles/emacs/init.el ~/.emacs.d/
 rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
