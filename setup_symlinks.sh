0be6c2a6ac3f3f5c0e486032ef5092355faed0ce
Jeff Parsons <parsoj@gmail.com>
2016-12-13 14:30:38 -0800

    fix linking script for spacemacs config

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,4 +1,4 @@
-rm ~/.spacemacs ; ln -s ~/.dotfiles/spacemacs/spacemacs_config
+rm ~/.spacemacs ; ln -s ~/.dotfiles/spacemacs/spacemacs_config ~/.spacemacs
 rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 rm -rf ~/.hammerspoon ; ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
