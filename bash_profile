959e1701705c23649889f921cfc36db683587901
Jeff Parsons <parsoj@gmail.com>
2017-02-01 21:16:34 -0800

    add bash_profile to VC'ed dotfiles

---
+++ bash_profile
@@ -0,0 +1,7 @@
+#homebrew stuff
+export PATH=/user/local/bin:$PATH
+
+#golang stuff
+export GOPATH=/Users/Jeff/code/go
+export PATH=$GOPATH/bin:$PATH
+
--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -2,5 +2,6 @@ rm ~/.spacemacs ; ln -s ~/.dotfiles/spacemacs/spacemacs_config ~/.spacemacs
 rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 rm -rf ~/.hammerspoon ; ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
+rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 
 
