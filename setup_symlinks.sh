f96136c9b832a72662bbe4a2367dab0bbef76e65
Jeff Parsons <parsoj@gmail.com>
2017-04-09 19:22:34 -0700

    add ssh config

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -3,5 +3,6 @@ rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 rm -rf ~/.hammerspoon ; ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
 rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+rm ~/.ssh/config ; ln -s ~/.dotfiles/ssh_config ~/.ssh/config
 
 
