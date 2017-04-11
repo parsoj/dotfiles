9e29d24fd25dc4f80e2771d0183672c0fd26c7d0
Jeff Parsons <parsoj@gmail.com>
2017-04-10 21:16:29 -0700

    add pre-net script; some cleanup

--- brew_installs.sh
+++ brew_installs.sh
@@ -24,7 +24,7 @@ brew install python
 brew install gnupg2
 
 # generate a new ssh key
-ssh-keygen -t rsa -b 4096 -N '' -C $EMAIL 
+ssh-keygen -t rsa -b 4096 -N '' -C $EMAIL
 
 # default curl on OSX is insecure - smush it
 brew install curl --with-openssl
@@ -33,7 +33,7 @@ brew link --force curl
 
 ################################################################################
 # cask -- fixing mac-ness
-brew cask install Alfred 
+brew cask install Alfred
 brew cask install seil
 
 ################################################################################
---
+++ pre-network-fixes.sh
@@ -0,0 +1,64 @@
+
+################################################################################
+# FileVault
+
+#$ cat > /dev/random
+#[Type random letters for a long while, then press Control-D]
+
+# enable FileVault
+sudo fdesetup enable
+
+# force hibernation + destroy FileVault key on standby
+sudo pmset -a destroyfvkeyonstandby 1
+sudo pmset -a hibernatemode 25
+
+# needed to prevent above settings from resulting in
+# auto-wake & then shutdown (since FV key is missing)
+sudo pmset -a powernap 0
+sudo pmset -a standby 0
+sudo pmset -a standbydelay 0
+sudo pmset -a autopoweroff 0
+
+################################################################################
+# Firewall stuff
+
+## Application-layer firewall
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+
+### disable auto-whitelisting of built-in or signed binaries/Apps
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
+
+### restart app-level firewall to take settings
+sudo pkill -HUP socketfilterfw
+
+
+## kernel-level firewall (just use pre-prepped file for now)
+sudo pfctl -e -f pf.rules #FIXME fix this path
+
+### INSTRUCTIONS:
+### Use the following commands to use the firewall:
+### sudo pfctl -e -f pf.rules to enable the firewall
+### sudo pfctl -d to disable the firewall
+### sudo pfctl -t blocklist -T add 1.2.3.4 to add hosts to a blocklist
+### sudo pfctl -t blocklist -T show to view the blocklist
+### sudo ifconfig pflog0 create to create an interface for logging
+### sudo tcpdump -ni pflog0 to dump the packets
+
+
+################################################################################
+# Disable OSX services
+## FIXME
+
+
+## disable spotlight suggestions
+### FIXME
+
+
+
+################################################################################
+# OSX usage preferences
+#FIXME dock autohiding, smaller size, on right
+#FIXME set caps lock key to "No Action" in keyboard prefs
