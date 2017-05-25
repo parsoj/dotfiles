f32e056d4881333d25821e00331b63f3da96c3da
Jeff Parsons <parsoj@gmail.com>
2017-05-24 18:37:42 -0700

    fit python style init

--- languages/python/init.sh
+++ languages/python/init.sh
@@ -1,5 +1,5 @@
 
 # styles
-rm -f ~/.config/flake8 ; ln -s ./styls/python/flake8 ~/.config/flake8
-rm -rf ~/.config/yapf ; ln -s ./style/python/yapf ~/.config/yapf
+rm -f ~/.config/flake8 ; ln -s ./style/flake8 ~/.config/flake8
+rm -rf ~/.config/yapf ; ln -s ./style/yapf ~/.config/yapf
 
