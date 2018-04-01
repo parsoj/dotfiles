6d8b07a185a8b494761e53d65dbd511249155ed9
Jeff Parsons <parsoj@gmail.com>
2018-04-01 09:23:47 -0700

    move symlinker to general bootstrap directory

--- bootstrap/symlink_configs.sh
+++ bootstrap/symlink_configs.sh
@@ -5,12 +5,9 @@ rm ~/.spacemacs ; ln -s ~/.dotfiles/spacemacs/spacemacs_config ~/.spacemacs
 rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 
-# Hammerspoon
-rm -rf ~/.hammerspoon ; ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
-
 # Bash
-rm -rf ~/.bashrc ; ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+#rm -rf ~/.bashrc ; ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+#rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
 # ssh
 rm ~/.ssh/config ; ln -s ~/.dotfiles/ssh/config ~/.ssh/config
