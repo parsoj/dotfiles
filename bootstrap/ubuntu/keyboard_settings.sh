b6d4f15374ce267ab426edf46177ea8ce202cac4
Jeff Parsons <parsoj@gmail.com>
2018-04-01 15:58:19 -0700

    move dconf keyboard settings notes to actual gsettings commands

--- bootstrap/ubuntu/keyboard_settings.sh
+++ bootstrap/ubuntu/keyboard_settings.sh
@@ -2,33 +2,25 @@
 # Key Remaps
 
 # setting capslock as another escape
-/org/gnome/desktop/input-sources/xkb-options
-['caps:escape']
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 
 ################################################################################
 # Keyboard shortcuts
 
 # workspace navigation shortcuts
-/org/gnome/desktop/wm/keybindings/switch-to-workspace-up
-['<Alt>k']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Alt>k']"
 
-/org/gnome/desktop/wm/keybindings/switch-to-workspace-down
-['<Alt>j']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Alt>j']"
 
-/org/gnome/desktop/wm/keybindings/switch-to-workspace-left
-['<Alt>h']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>h']"
 
-/org/gnome/desktop/wm/keybindings/switch-to-workspace-right
-['<Alt>l']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>l']"
 
 # window control shortcuts
 
-/org/gnome/desktop/wm/keybindings/toggle-maximized
-['<Super>u']
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>u']"
 
-/org/gnome/mutter/keybindings/toggle-tiled-left
-['<Super>h']
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>h']"
 
-/org/gnome/mutter/keybindings/toggle-tiled-right
-['<Super>l']
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>l']"
 
