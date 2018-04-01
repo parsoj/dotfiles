2a5463d844f34202c8c94d4ad8340ba51986fe02
Jeff Parsons <parsoj@gmail.com>
2018-04-01 14:46:14 -0700

    add initial keyboard settings and login items configs

---
+++ bootstrap/ubuntu/keyboard_settings.sh
@@ -0,0 +1,34 @@
+################################################################################
+# Key Remaps
+
+# setting capslock as another escape
+/org/gnome/desktop/input-sources/xkb-options
+['caps:escape']
+
+################################################################################
+# Keyboard shortcuts
+
+# workspace navigation shortcuts
+/org/gnome/desktop/wm/keybindings/switch-to-workspace-up
+['<Alt>k']
+
+/org/gnome/desktop/wm/keybindings/switch-to-workspace-down
+['<Alt>j']
+
+/org/gnome/desktop/wm/keybindings/switch-to-workspace-left
+['<Alt>h']
+
+/org/gnome/desktop/wm/keybindings/switch-to-workspace-right
+['<Alt>l']
+
+# window control shortcuts
+
+/org/gnome/desktop/wm/keybindings/toggle-maximized
+['<Super>u']
+
+/org/gnome/mutter/keybindings/toggle-tiled-left
+['<Super>h']
+
+/org/gnome/mutter/keybindings/toggle-tiled-right
+['<Super>l']
+
