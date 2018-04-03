51ea833d059b5b3c6bfd33d0e132a99921a73c12
Jeff Parsons <parsoj@gmail.com>
2018-04-03 11:25:31 -0700

    remove non-functional albert install

--- bootstrap/ubuntu/installations.sh
+++ bootstrap/ubuntu/installations.sh
@@ -21,13 +21,6 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key ad
 sudo apt update
 sudo apt install google-chrome-stable
 
-# Albert
-wget -nv -O Release.key https://build.opensuse.org/projects/home:manuelschneid3r/public_key
-sudo apt-key add - < Release.key
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
-sudo apt update
-sudo apt install albert
-
 # gnome tweak tool
 # (can check dconf settings changedwith `dconf watch /` to move to gsettings)
 sudo apt install gnome-tweak-tool
