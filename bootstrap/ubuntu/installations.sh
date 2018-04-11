5f9f854dbbdcabc198f04db1f31e2a02278137e2
Jeff Parsons <parsoj@gmail.com>
2018-04-11 08:09:43 -0700

    add missing line to google-chrome bootstrapper

--- bootstrap/ubuntu/installations.sh
+++ bootstrap/ubuntu/installations.sh
@@ -18,6 +18,7 @@ sudo apt install markdown
 
 # Chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update
 sudo apt install google-chrome-stable
 
