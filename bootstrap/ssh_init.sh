60f82a3b27d45dc46ddbfb720b26f4e606dc868a
Jeff Parsons <parsoj@gmail.com>
2018-04-14 11:24:54 -0700

    rename to ssh init

--- bootstrap/ssh_init.sh
+++ bootstrap/ssh_init.sh
@@ -1,3 +1,3 @@
-ssh-keygen -t rsa -b 4096 -N "" -C "parsoj@gmail.com" -f ~/.ssh/github.key
+ssh-keygen -t rsa -b 4096 -N "" -C "parsoj@gmail.com"
 sudo apt install git
 echo "please sign into github and register your ~/.ssh/github.pub"
