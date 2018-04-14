6e9113ad1c8aa9c959889553b361642a1bb40259
Jeff Parsons <parsoj@gmail.com>
2018-04-14 11:26:35 -0700

    refactor ubuntu bootstrap files

--- bootstrap/ubuntu/applications/rescuetime/install.sh
+++ bootstrap/ubuntu/applications/rescuetime/install.sh
@@ -2,6 +2,12 @@
 # base install
 wget https://www.rescuetime.com/installers/rescuetime_current_amd64.deb
 sudo dpkg -i rescuetime_current_amd64.deb
+sudo apt update
+sudo apt -y install rescuetime
+sudo apt -f install
+
+rm rescuetime_current_amd64.deb
 
 # start rescuetime on login
+mkdir -p ~/.config/autostart
 cp rescuetime.desktop ~/.config/autostart/
--- bootstrap/ubuntu/applications/spacemacs/install.sh
+++ bootstrap/ubuntu/applications/spacemacs/install.sh
@@ -17,9 +17,16 @@ cp ~/.dotfiles/spacemacs/fonts/all-the-icons/*.ttf ~/.fonts
 # Run Spacemacs as a Daemon
 
 # run emacs daemon (copy autostart file into autostart dir)
+mkdir -p ~/.config/autostart
 cp emacs.desktop ~/.config/autostart/
 
 # set up emacs client launcher (using gsettings to edit gnome dconf)
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'EmacsClient Launcher'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>e'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'emacsclient -c -e \"(shell)\"'"
+
+################################################################################
+
+# spacemacs support utilities
+sudo apt -y install silversearcher-ag
+sudo apt -y install markdown
