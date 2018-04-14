751e40a3917a21d64fe11d5701445784bcfd6f01
Jeff Parsons <parsoj@gmail.com>
2018-04-14 11:27:48 -0700

    add installers to python language support

--- spacemacs/language_support/python/init.sh
+++ spacemacs/language_support/python/init.sh
@@ -1,4 +1,12 @@
 
+sudo apt -y install python-dev
+sudo apt -y install python-pip
+pip install virualenv
+
+sudo apt -y install python3-dev
+sudo apt -y install python3-pip
+
+
 # styles
 rm -f ~/.config/flake8 ; ln -s ./style/flake8 ~/.config/flake8
 rm -rf ~/.config/yapf ; ln -s ./style/yapf ~/.config/yapf
