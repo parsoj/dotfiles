8c82c65ce796412006112d801e754a200158bd9f
Jeff Parsons <parsoj@gmail.com>
2018-04-14 11:27:31 -0700

    symlink bash as well

--- bootstrap/symlink_configs.sh
+++ bootstrap/symlink_configs.sh
@@ -6,5 +6,5 @@ rm ~/.gitconfig ; ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 rm ~/.gitignore_global ; ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 
 # Bash
-#rm -rf ~/.bashrc ; ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-#rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+rm -rf ~/.bashrc ; ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
