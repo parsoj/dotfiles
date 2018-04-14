9e9755e44a5b4f6d478d365ef4589d8d8877abb8
Jeff Parsons <parsoj@gmail.com>
2018-04-14 11:25:12 -0700

    remove flux (since it already is included in gnome)

--- bootstrap/ubuntu/applications/flux/flux_install.sh
+++
@@ -1,9 +0,0 @@
-
-# install flux
-sudo add-apt-repository ppa:nathan-renniewaldock/flux
-sudo apt update
-sudo apt install fluxgui
-
-# start flux on login
-cp fluxgui.desktop ~/.config/autostart/
-
