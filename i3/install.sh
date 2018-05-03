e506401908960d64ed9efdaac4f8c312d862e6be
Jeff Parsons <parsoj@gmail.com>
2018-05-03 08:52:55 -0700

    add initial i3 install script

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,7 @@
 
+sudo apt -y install i3
+
 # link in config
-mkdir -P ~/.config/i3
+mkdir -p ~/.config/i3
 rm -rf ~/.config/i3/config ; ln -s ~/.dotfiles/i3/config ~/.config/i3/config
 
-sudo apt -y install i3
