d8484831c406e6c705925711cbd6547a2ff71688
Jeff Parsons <parsoj@gmail.com>
2018-05-07 08:45:48 -0700

    add feh to i3 installer script

--- i3/i3_config.sh
+++ i3/i3_config.sh
@@ -5,5 +5,8 @@ sudo apt -y install xbacklight
 # setup for trackpad
 sudo apt -y install xserver-xorg-input-synaptics
 
-sudo apt -u install blueman
+sudo apt -y install blueman
+
+# used to set desktop background
+sudo apt -y install feh
 
