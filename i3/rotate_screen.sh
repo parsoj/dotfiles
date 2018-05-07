bcf2466d73eb751d284c80f21b2e460699ec26c9
Jeff Parsons <parsoj@gmail.com>
2018-05-07 08:48:27 -0700

    add temp fix for screen rotation script

--- i3/rotate_screen.sh
+++ i3/rotate_screen.sh
@@ -7,21 +7,31 @@ TRANSFORM_TYPE='Coordinate Transformation Matrix'
 DIRECTION=$1
 
 # pick the transformation matrix for our rotation
+#case "$DIRECTION" in
+#    "normal") TRANS_MATRIX="1 0 0 0 1 0 0 0 1";;
+#    "inverted") TRANS_MATRIX="-1 0 1 0 -1 1 0 0 1";;
+#    "left") TRANS_MATRIX="0 -1 1 1 0 0 0 0 1";;
+#    "right") TRANS_MATRIX="0 1 0 -1 0 1 0 0 1";;
+#esac
+
+
 case "$DIRECTION" in
-    "normal") TRANS_MATRIX="1 0 0 0 1 0 0 0 1";;
-    "inverted") TRANS_MATRIX="-1 0 1 0 -1 1 0 0 1";;
-    "left") TRANS_MATRIX="0 -1 1 1 0 0 0 0 1";;
-    "right") TRANS_MATRIX="0 1 0 -1 0 1 0 0 1";;
+    "normal") TRANS=0;;
+    "inverted") TRANS=3;;
+    "left") TRANS=2;;
+    "right") TRANS=1;;
 esac
 
 # perform the rotation for all relevant input devices
 for DEVICE in "${INPUT_DEVICES[@]}"
 do
     echo "rotating input device: \"$DEVICE\""
-    xinput set-prop "$DEVICE" "$TRANSFORM_TYPE" $TRANS_MATRIX
+    #xinput set-prop "$DEVICE" "$TRANSFORM_TYPE" $TRANS_MATRIX
+    xsetwacom --set "$DEVICE" Rotate $TRANS
 done
 
 # perform the rotation on the display
-xrandr --output eDP-1 --rotate $DIRECTION
+#xrandr --output eDP-1 --rotate $DIRECTION
+xrandr --output eDP1 --rotate $DIRECTION
 
 
