4898eacf0497756b8597279017ae74f5eb3f9997
Jeff Parsons <parsoj@gmail.com>
2018-05-19 11:25:31 -0700

    install golang godoc

--- spacemacs/language_support/golang/install.sh
+++ spacemacs/language_support/golang/install.sh
@@ -2,6 +2,7 @@
 sudo apt -y install install go
 
 # tools for spacemacs go layer
+go get -u -v golang.org/x/tools/cmd/godoc
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
