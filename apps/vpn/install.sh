8014a3bb49429383e05b4b8607a19d40a210bc88
Jeff Parsons <parsoj@gmail.com>
2018-05-24 18:27:31 -0700

    install network manager components for VPN

--- apps/vpn/install.sh
+++ apps/vpn/install.sh
@@ -4,16 +4,18 @@ set -e
 
 # this should already be installed - but its a dependency for using Nord VPN
 sudo apt -y install ca-certificates
-
 sudo apt -y install openvpn
 
-cd /etc/openvpn
 
 # Download NordVPN OpenVpn config files
+cd /etc/openvpn
 sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
-
 sudo unzip ovpn.zip
-
 sudo rm ovpn.zip
 
+# install the nm component for openvpn
+sudo apt-get install network-manager-openvpn
+sudo apt-get install network-manager-openvpn-gnome
+
+sudo systemctl restart network-manager
 
