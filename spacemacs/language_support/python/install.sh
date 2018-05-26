e7bb0810eadaafac0c27fd864a27d179fcd2df0e
Jeff Parsons <parsoj@gmail.com>
2018-05-26 08:51:32 -0700

    actually install flake8 and yapf as part of python install

--- spacemacs/language_support/python/install.sh
+++ spacemacs/language_support/python/install.sh
@@ -9,7 +9,10 @@ sudo apt -y install python3-pip
 pip3 install ipython
 
 
-# styles
+# auto formatting and style checking
 rm -f ~/.config/flake8 ; ln -s ./style/flake8 ~/.config/flake8
+pip install flake8
+
 rm -rf ~/.config/yapf ; ln -s ./style/yapf ~/.config/yapf
+pip install yapf
 
