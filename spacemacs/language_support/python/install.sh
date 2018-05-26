d9b7a4bc9eac360476a6b30058ccf3e842c0bb89
Jeff Parsons <parsoj@gmail.com>
2018-05-26 09:15:56 -0700

    fix config file locations for python formatters/stylers

--- spacemacs/language_support/python/install.sh
+++ spacemacs/language_support/python/install.sh
@@ -10,9 +10,9 @@ pip3 install ipython
 
 
 # auto formatting and style checking
-rm -f ~/.config/flake8 ; ln -s ./style/flake8 ~/.config/flake8
+rm -f ~/.config/flake8 ; ln -s ~/.dotfiles/spacemacs/language_support/python/flake8  ~/.config/flake8
 pip install flake8
 
-rm -rf ~/.config/yapf ; ln -s ./style/yapf ~/.config/yapf
+rm -rf ~/.config/yapf ; ln -s ~/.dotfiles/spacemacs/language_support/python/yapf ~/.config/yapf
 pip install yapf
 
