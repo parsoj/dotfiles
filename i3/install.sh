0ea2bd46c174235865464d6b44284395cf12a9c1
Jeff Parsons <parsoj@gmail.com>
2018-06-27 14:39:51 -0700

    add i3status config

--- i3/install.sh
+++ i3/install.sh
@@ -1,5 +1,7 @@
 
-# link in config
+# link in configs
 mkdir -p ~/.i3
 rm -rf ~/.i3/config ; ln -s ~/.dotfiles/i3/config ~/.i3/config
+mkdir -p ~/.config/i3status
+rm -rf ~/.config/i3status/config ; ln -s ~/.dotfiles/i3/i3status ~/.config/i3status/config
 
