1162dc7d4dfddefb97c6d63a635e63c6249a5be5
Jeff Parsons <parsoj@gmail.com>
2018-06-27 14:40:05 -0700

    install noto fonts for i3status bar

--- i3/install.sh
+++ i3/install.sh
@@ -5,3 +5,8 @@ rm -rf ~/.i3/config ; ln -s ~/.dotfiles/i3/config ~/.i3/config
 mkdir -p ~/.config/i3status
 rm -rf ~/.config/i3status/config ; ln -s ~/.dotfiles/i3/i3status ~/.config/i3status/config
 
+# install noto emoji fonts for i3 status bar
+# yaourt -S noto-fonts-emoji
+
+
+
