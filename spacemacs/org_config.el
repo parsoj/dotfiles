4627228707d151c5721759f8918622ba2e34192b
Jeff Parsons <parsoj@gmail.com>
2018-08-05 12:10:03 -0700

    remove unused helper functions

--- spacemacs/org_config.el
+++ spacemacs/org_config.el
@@ -36,44 +36,6 @@
 (setq org-columns-default-format "%50ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM %16TIMESTAMP_IA")
 
 ;;******************************************************************************************
-;; bh/helper-functions
-(defun bh/is-project-p ()
-  "Any task with a todo keyword subtask."
-  (save-restriction
-    (widen)
-    (let ((has-subtask)
-          (subtree-end (save-excursion (org-end-of-subtree t)))
-          (is-a-task (member (nth 2 (org-heading-components)) org-todo-keywords-1)))
-      (save-excursion
-        (forward-line 1)
-        (while (and (not has-subtask)
-                    (< (point) subtree-end)
-                    (re-search-forward "^\*+ " subtree-end t))
-          (when (member (org-get-todo-state) org-todo-keywords-1)
-            (setq has-subtask t))))
-      (and is-a-task has-subtask))))
-(defun bh/find-project-task ()
-  "Move point to the parent (project) task if any."
-  (save-restriction
-    (widen)
-    (let ((parent-task (save-excursion (org-back-to-heading 'invisible-ok) (point))))
-      (while (org-up-heading-safe)
-        (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
-          (setq parent-task (point))))
-      (goto-char parent-task)
-      parent-task)))
-(defun bh/is-project-subtree-p ()
-  "Any task with a todo keyword that is in a project subtree.
-Callers of this function already widen the buffer view."
-  (let ((task (save-excursion (org-back-to-heading 'invisible-ok)
-                              (point))))
-    (save-excursion
-      (bh/find-project-task)
-      (if (equal (point) task)
-          nil
-        t))))
-
-;;******************************************************************************************
 ;; Tags
 (setq org-tag-alist '((:startgroup)
 		      ("@errands" . ?e)
@@ -236,94 +198,9 @@ Callers of this function already widen the buffer view."
 ;; Compact the block agenda view (disabled)
 (setq org-agenda-compact-blocks nil)
 
-;; Set the times to display in the time grid
-(setq org-agenda-time-grid
-  '((daily today require-timed)
-    "----------------"
-    (800 1200 1600 2000)))
-
-;; Some helper functions for selection within agenda views
-(defun gs/select-with-tag-function (select-fun-p)
-  (save-restriction
-    (widen)
-    (let ((next-headline
-	   (save-excursion (or (outline-next-heading)
-			       (point-max)))))
-      (if (funcall select-fun-p) nil next-headline))))
-
-(defun gs/select-projects ()
-  "Selects tasks which are project headers"
-  (gs/select-with-tag-function #'bh/is-project-p))
-(defun gs/select-project-tasks ()
-  "Skips tags which belong to projects (and is not a project itself)"
-  (gs/select-with-tag-function
-   #'(lambda () (and
-		 (not (bh/is-project-p))
-		 (bh/is-project-subtree-p)))))
-(defun gs/select-standalone-tasks ()
-  "Skips tags which belong to projects. Is neither a project, nor does it blong to a project"
-  (gs/select-with-tag-function
-   #'(lambda () (and
-		 (not (bh/is-project-p))
-		 (not (bh/is-project-subtree-p))))))
-;;(defun gs/select-projects-and-standalone-tasks ()
-;;  "Skips tags which are not projects"
-;;  (gs/select-with-tag-function
-;;   #'(lambda () (or
-;;		 (bh/is-project-p)
-;;		 (bh/is-project-subtree-p)))))
-
-(defun gs/org-agenda-project-warning ()
-  "Is a project stuck or waiting. If the project is not stuck,
-show nothing. However, if it is stuck and waiting on something,
-show this warning instead."
-  (if (gs/org-agenda-project-is-stuck)
-    (if (gs/org-agenda-project-is-waiting) " !W" " !S") ""))
-
-(defun gs/org-agenda-project-is-stuck ()
-  "Is a project stuck"
-  (if (bh/is-project-p) ; first, check that it's a project
-      (let* ((subtree-end (save-excursion (org-end-of-subtree t)))
-	     (has-next))
-	(save-excursion
-	  (forward-line 1)
-	  (while (and (not has-next)
-		      (< (point) subtree-end)
-		      (re-search-forward "^\\*+ NEXT " subtree-end t))
-	    (unless (member "WAITING" (org-get-tags-at))
-	      (setq has-next t))))
-	(if has-next nil t)) ; signify that this project is stuck
-    nil)) ; if it's not a project, return an empty string
-
-(defun gs/org-agenda-project-is-waiting ()
-  "Is a project stuck"
-  (if (bh/is-project-p) ; first, check that it's a project
-      (let* ((subtree-end (save-excursion (org-end-of-subtree t))))
-	(save-excursion
-	  (re-search-forward "^\\*+ WAITING" subtree-end t)))
-    nil)) ; if it's not a project, return an empty string
-
-;; Some helper functions for agenda views
-(defun gs/org-agenda-prefix-string ()
-  "Format"
-  (let ((path (org-format-outline-path (org-get-outline-path))) ; "breadcrumb" path
-	(stuck (gs/org-agenda-project-warning))) ; warning for stuck projects
-       (if (> (length path) 0)
-	   (concat stuck ; add stuck warning
-		   " [" path "]") ; add "breadcrumb"
-	 stuck)))
-
-(defun gs/org-agenda-add-location-string ()
-  "Gets the value of the LOCATION property"
-  (let ((loc (org-entry-get (point) "LOCATION")))
-    (if (> (length loc) 0)
-	(concat "{" loc "} ")
-      "")))
-
-;; Variables for ignoring tasks with deadlines
-(defvar gs/hide-deadline-next-tasks t)
+;; ignore stuff that is scheduled in the future
+(setq org-agenda-todo-ignore-scheduled 'future)
 (setq org-agenda-tags-todo-honor-ignore-options t)
-(setq org-deadline-warning-days 10)
 
 ;; Custom agenda command definitions
 (setq org-agenda-custom-commands
