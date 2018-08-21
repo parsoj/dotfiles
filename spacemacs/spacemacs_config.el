66e1abf566d17790bbc869e9275d783b5875400b
Jeff Parsons <parsoj@gmail.com>
2018-08-21 10:10:42 -0700

    use material theme

--- spacemacs/spacemacs_config.el
+++ spacemacs/spacemacs_config.el
@@ -32,6 +32,7 @@ values."
    dotspacemacs-configuration-layers
    '(
      php
+     themes-megapack
      (latex :variables latex-build-command "xelatex")
      javascript
      restclient
@@ -158,8 +159,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(material
+                         spacemacs-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
