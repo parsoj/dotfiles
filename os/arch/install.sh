94dfb9b88b1d701f5d563111b869390532bb4b33
Jeff Parsons <parsoj@gmail.com>
2018-09-01 13:59:24 -0700

    fix touchpad conf installer

--- os/arch/install.sh
+++ os/arch/install.sh
@@ -1,4 +1,4 @@
-sudo ln -s ~/.dotfiles/arch/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo rm /etc/X11/xorg.conf.d/30-touchpad.conf; sudo ln -s ~/.dotfiles/os/arch/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 
-yaourt -S xf86-input-synaptics
+#yaourt -S xf86-input-synaptics
 
