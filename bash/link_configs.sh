162c6ddb57f3ae54adb764436ede4c22053f6a0e
Jeff Parsons <parsoj@gmail.com>
2018-09-01 12:49:05 -0700

    move bash to shell sub-dir and clean up

--- bash/link_configs.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ~/.bashrc ; ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
---
+++ shell/bash/link_configs.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.bashrc ; ln -s ~/.dotfiles/shell/bash/bashrc ~/.bashrc
+rm -rf ~/.bash_profile ; ln -s ~/.dotfiles/shell/bash/bash_profile ~/.bash_profile
