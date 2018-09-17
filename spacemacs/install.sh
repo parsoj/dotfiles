f60df449d25be7286564e03d1917b8c3deef4492
Jeff Parsons <parsoj@gmail.com>
2018-09-17 12:29:22 -0700

    use yay for spacemacs bootstrapping; clean up comments

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,10 +1,8 @@
-
 #############################################################
-# Spacemacs installation
+# Spacemacs
 
 # base install
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-pacman -S emacs
 
 # link in spacemacs config
 rm ~/.spacemacs ; ln -s ~/.dotfiles/spacemacs/spacemacs_config.el ~/.spacemacs
@@ -17,15 +15,12 @@ cp ~/.dotfiles/spacemacs/fonts/source-code-pro/*.ttf ~/.fonts
 cp ~/.dotfiles/spacemacs/fonts/all-the-icons/*.ttf ~/.fonts
 
 ##############################################################
-# base emacs install
+# Emacs
 
-# add kevin kelley's emacs ppa
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt update
-sudo apt install emacs26
+yay -S emacs26-git
 
 ################################################################################
-# set up systemd to launch emacs daemon on startup
+# run Emacs as a deamon on startup
 
 mkdir -p ~/.config/systemd/user
 rm ~/.config/systemd/user/emacs.service ; ln -s /home/jeff/.dotfiles/spacemacs/systemd/emacs.service ~/.config/systemd/user/emacs.service
@@ -34,13 +29,7 @@ systemctl enable --user emacs
 systemctl start --user emacs
 
 ################################################################################
-
-# spacemacs support utilities
-pacaur -S the_silver_searcher
-pacaur -S markdown
-pacaur -S global
-################################################################################
- # random utils
-
-# pops up a notification when long-running scripts complete (accompanied with .bashrc setting)
-pacaur -S undistract-me-git
+# Spacemacs support utilities
+yay -S the_silver_searcher
+yay -S markdown
+yay -S global
