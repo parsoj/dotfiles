f233f4c055b184e3a77a0691744693dc3acb11af
Jeff Parsons <parsoj@gmail.com>
2018-09-17 15:04:03 -0700

    update i3 config linker to reflect new config setup

--- i3/install.sh
+++ i3/install.sh
@@ -2,11 +2,6 @@
 # link in configs
 mkdir -p ~/.i3
 rm -rf ~/.i3/config ; ln -s ~/.dotfiles/i3/config ~/.i3/config
-mkdir -p ~/.config/i3status
-rm -rf ~/.config/i3status/config ; ln -s ~/.dotfiles/i3/i3status ~/.config/i3status/config
-
-# install noto emoji fonts for i3 status bar
-# pacaur -S noto-fonts-emoji
 
 
 
