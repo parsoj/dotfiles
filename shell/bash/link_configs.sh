a81e3bdf72269fde5c9b5e0a6cd0b2772c51715c
Jeff Parsons <parsoj@gmail.com>
2018-09-17 12:25:45 -0700

    add comments to bash linker; install undistract me with bash install

--- shell/bash/link_configs.sh
+++ shell/bash/link_configs.sh
@@ -1,1 +1,6 @@
+
+# link over bashrc
 rm -rf ~/.bashrc; ln -s ~/.dotfiles/shell/bash/bashrc ~/.bashrc
+
+# install the undistract-me notifier for long-running bash commands
+yay -S undistract-me-git
