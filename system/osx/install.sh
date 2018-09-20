119c61e1133ffb69da46666319ae12e4b63dd9c8
Jeff Parsons <parsoj@gmail.com>
2018-09-19 22:55:00 -0700

    add bitbar indicator

--- system/osx/install.sh
+++ system/osx/install.sh
@@ -9,6 +9,10 @@ brew tap crisidev/homebrew-chunkwm
 brew install chunkwm
 brew services start chunkwm
 
+# bitbar plugin for chunkwm
+ln -s ~/.dotfiles/system/osx/chunkwm_skhd.1s.sh ~/.bitbar/plugins/
+brew cask install bitbar
+
 #skhd
 ln -s ~/.dotfiles/system/osx/skhdrc ~/.skhdrc
 brew install koekeishiya/formulae/skhd
