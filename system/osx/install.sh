4d0781af67f250192a09e6a601371adc2769177a
Jeff Parsons <parsoj@gmail.com>
2018-09-25 10:24:32 -0700

    add initial brew installs

--- system/osx/install.sh
+++ system/osx/install.sh
@@ -18,6 +18,36 @@ ln -s ~/.dotfiles/system/osx/skhdrc ~/.skhdrc
 brew install koekeishiya/formulae/skhd
 brew services start skhd
 
+################################################################################
+# Apps install
+brew cask install google-chrome
+
+# communication
+brew cask install slack
+
+# entertainment
+brew cask install spotify
+
+# dev tools
+brew cask install postman
+brew cask install docker
+
+# productivity
+brew cask install dropbox
+brew cask install evernote
+brew cask install rescuetime
+brew cask install fantastical
+
+# system fix tools
+brew cask install 1password
+brew cask install bartender
+brew cask install alfred
+brew cask install bitbar
+brew cask install istat-menus
+
+
+
+
 
 
 
