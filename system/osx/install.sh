ccdfac1b890d29a5794384f544c8386913b481d3
Jeff Parsons <parsoj@gmail.com>
2018-10-14 15:57:21 -0700

    stop crappy design choice of randomly stealing focus

--- system/osx/install.sh
+++ system/osx/install.sh
@@ -80,3 +80,9 @@ brew cask install bartender
 brew cask install alfred
 brew cask install istat-menus
 brew install coreutils
+
+##########################################################################################
+# fix some really dumb default behavior in OSX
+
+# don't randomly redirect focus to other workspaces
+defaults write com.apple.dock workspaces-auto-swoosh -bool NO
