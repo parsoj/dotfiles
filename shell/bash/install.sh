0830bed5453e0420d2845f27edbdaa57db9d27da
Jeff Parsons <parsoj@gmail.com>
2018-11-28 11:12:51 -0800

    fix shell configs

--- shell/bash/install.sh
+++
@@ -1,6 +0,0 @@
-
-# link over bashrc
-rm -rf ~/.bashrc; ln -s ~/.dotfiles/shell/bash/bashrc ~/.bashrc
-
-# install the undistract-me notifier for long-running bash commands
-# yay -S undistract-me-git
---
+++ shell/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ~/.bashrc; ln -s ~/.dotfiles/shell/bashrc ~/.bashrc
+rm -rf ~/.bash_profile; ln -s ~/.dotfiles/shell/bash_profile ~/.bash_profile
