93d9eaa85fa4606c5b574bd21cecb31229bd52c4
Jeff Parsons <parsoj@gmail.com>
2018-12-01 20:11:37 -0800

    separate out, but keep old OSX configs

---
+++ osx/install.sh
@@ -0,0 +1,7 @@
+
+
+##########################################################################################
+# fix some really dumb default behavior in OSX
+
+# don't randomly redirect focus to other workspaces
+defaults write com.apple.dock workspaces-auto-swoosh -bool NO
