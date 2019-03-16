72f007911783d071bdeef01bb17b151659387e46
Jeff Parsons <parsoj@gmail.com>
2019-03-16 08:21:56 -0700

    add vagrant and docker layers

--- spacemacs/spacemacs_config.el
+++ spacemacs/spacemacs_config.el
@@ -34,6 +34,8 @@ This function should only modify configuration layer settings."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     vagrant
+     docker
      slack
      restclient
      php
