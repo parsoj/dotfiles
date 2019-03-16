6beace3fccecc5953812758ddf17bf866f008f96
Jeff Parsons <parsoj@gmail.com>
2019-03-16 10:26:42 -0700

    clean out old installer scripts

--- osx/osx_settings.sh
+++
@@ -1,7 +0,0 @@
-
-
-##########################################################################################
-# fix some really dumb default behavior in OSX
-
-# don't randomly redirect focus to other workspaces
-defaults write com.apple.dock workspaces-auto-swoosh -bool NO
--- shell/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf ~/.bashrc; ln -s ~/.dotfiles/shell/bashrc ~/.bashrc
-rm -rf ~/.bash_profile; ln -s ~/.dotfiles/shell/bash_profile ~/.bash_profile
