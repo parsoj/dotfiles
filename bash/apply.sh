461e3126727ff5393c40158771a816b056a711de
Jeff Parsons <parsoj@gmail.com>
2019-04-22 18:19:07 -0700

    cleanup config apply scripts

---
+++ bash/apply.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.bashrc; ln -s ~/.config/shell/bashrc ~/.bashrc
+rm -rf ~/.bash_profile; ln -s ~/.config/shell/bash_profile ~/.bash_profile
---
+++ osx/apply.sh
@@ -0,0 +1,17 @@
+
+###############################################################################
+# fix some really dumb default behavior in OSX
+
+# don't randomly redirect focus to other workspaces
+defaults write com.apple.dock workspaces-auto-swoosh -bool NO
+
+
+###############################################################################
+# Symlink configs into place
+
+rm ~/.skhdrc ; ln -s  ~/.config/osx/skhdrc ~/.skhdrc
+rm ~/.chunkwmrc ; ln -s  ~/.config/osx/chunkwmrc ~/.chunkwmrc
+
+
+###############################################################################
+# Apply Brewfile
