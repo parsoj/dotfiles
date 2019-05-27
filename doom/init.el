25f4afceaba2a804e65804829176b35048694ecf
Jeff Parsons <parsoj@gmail.com>
2019-05-27 11:15:58 -0700

    use the fira ligatures

--- doom/init.el
+++ doom/init.el
@@ -38,7 +38,9 @@
  (popup            ; tame sudden yet inevitable temporary windows
   +all             ; catch all popups that start with an asterix
   +defaults)       ; default popup rules
- pretty-code       ; replace bits of code with pretty symbols
+ (pretty-code
+  +fira
+  )       ; replace bits of code with pretty symbols
  unicode           ; extended unicode support for various languages
  vc-gutter         ; vcs diff in the fringe
  window-select     ; visually switch windows
