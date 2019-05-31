625dd7ef9cc9d63e9509bf2afcb60412e928f1b5
Jeff Parsons <parsoj@gmail.com>
2019-05-30 18:34:46 -0700

    commit init state

--- doom/init.el
+++ doom/init.el
@@ -1,6 +1,7 @@
 ;;; init.el -*- lexical-binding: t; -*-
 ;; Copy me to ~/.doom.d/init.el or ~/.config/doom/init.el, then edit me!
 ;;
+;;
 
 (setq doom-localleader-key ",")
 
@@ -28,44 +29,45 @@
 
  :ui
  doom-one
- ;; doom-dashboard    ; a nifty splash screen for Emacs
+ doom-dashboard                         ; a nifty splash screen for Emacs
 
  ;;fill-column       ; a `fill-column' indicator
- hl-todo           ; highlight TODO/FIXME/NOTE tags
- modeline          ; snazzy, Atom-inspired modeline, plus API
- nav-flash         ; blink the current line after jumping
- treemacs          ; a project drawer, like neotree but cooler
- (popup            ; tame sudden yet inevitable temporary windows
-  +all             ; catch all popups that start with an asterix
-  +defaults)       ; default popup rules
+ hl-todo                          ; highlight TODO/FIXME/NOTE tags
+ modeline                         ; snazzy, Atom-inspired modeline, plus API
+ nav-flash                        ; blink the current line after jumping
+ treemacs                         ; a project drawer, like neotree but cooler
+ (popup                           ; tame sudden yet inevitable temporary windows
+  +all                            ; catch all popups that start with an asterix
+  +defaults)                      ; default popup rules
  (pretty-code
   +fira
-  )       ; replace bits of code with pretty symbols
- unicode           ; extended unicode support for various languages
- vc-gutter         ; vcs diff in the fringe
+  )                             ; replace bits of code with pretty symbols
+ unicode                        ; extended unicode support for various languages
+ vc-gutter                      ; vcs diff in the fringe
  (window-select
   +numbers
-  )     ; visually switch windows
+  )                                     ; visually switch windows
  indent-guides
  ophints
 
  :editor
- (evil +everywhere); come to the dark side, we have cookies
- workspaces        ; tab emulation, persistence & separate workspaces
+ (evil +everywhere)           ; come to the dark side, we have cookies
+ workspaces                   ; tab emulation, persistence & separate workspaces
  snippets
- fold              ; (nigh) universal code folding
- multiple-cursors  ; editing in many places at once
+ fold                                   ; (nigh) universal code folding
+ multiple-cursors                       ; editing in many places at once
  file-templates
- ;; lispy
+ lispy
  rotate-text
 
  :emacs
- (dired            ; making dired pretty [functional]
-  +ranger         ; bringing the goodness of ranger to dired
-  +icons          ; colorful icons for dired-mode
+ projects
+ (dired                               ; making dired pretty [functional]
+  +ranger                             ; bringing the goodness of ranger to dired
+  +icons                              ; colorful icons for dired-mode
   )
- electric          ; smarter, keyword-based electric-indent
- imenu             ; an imenu sidebar and searchable code index
+ electric                           ; smarter, keyword-based electric-indent
+ imenu                              ; an imenu sidebar and searchable code index
  term
  jeffp-eshell
 
@@ -74,43 +76,38 @@
  vagrant
  docker
  terraform
- flycheck          ; tasing you for every semicolon you forget
- flyspell          ; tasing you for misspelling mispelling
- gist              ; interacting with github gists
- (lsp
-  +lsp-mode
-  +lsp-ui
-  +company-lsp
-  )
- macos             ; MacOS-specific commands
- magit             ; a git porcelain for Emacs
- make              ; run make tasks from Emacs
+ flycheck                            ; tasing you for every semicolon you forget
+ flyspell                            ; tasing you for misspelling mispelling
+ gist                                ; interacting with github gists
+ lsp
+ macos                                  ; MacOS-specific commands
+ magit                                  ; a git porcelain for Emacs
+ make                                   ; run make tasks from Emacs
  (lookup
   +docsets
   )
  prodigy
 
  :lang
- emacs-lisp        ; drown in parentheses
- go                ; the hipster dialect
- kotlin            ; a better, slicker Java(Script)
- (java
-  +lsp
-  )
- markdown          ; writing docs for people to ignore
- (org              ; organize your plain life in plain text
-  +attach          ; custom attachment system
-  +babel           ; running code in org
-  +capture         ; org-capture in and outside of Emacs
-  +export          ; Exporting org to whatever you want
-  +present         ; Emacs for presentations
-  +protocol)       ; Support for org-protocol:// links
- perl              ; write code no one else can comprehend
+ emacs-lisp                             ; drown in parentheses
+ go                                     ; the hipster dialect
+ kotlin                                 ; a better, slicker Java(Script)
+ jeffp-java
+ markdown                               ; writing docs for people to ignore
+ xml
+ (org                                   ; organize your plain life in plain text
+  +attach                               ; custom attachment system
+  +babel                                ; running code in org
+  +capture                              ; org-capture in and outside of Emacs
+  +export                               ; Exporting org to whatever you want
+  +present                              ; Emacs for presentations
+  +protocol)                            ; Support for org-protocol:// links
+ perl                                   ; write code no one else can comprehend
  (php
   +lsp)               ; perl's insecure younger brother
- plantuml          ; diagrams for confusing people more
- rest              ; Emacs as a REST client
- ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+ plantuml             ; diagrams for confusing people more
+ rest                 ; Emacs as a REST client
+ ruby                 ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
 
  :app
  slack
