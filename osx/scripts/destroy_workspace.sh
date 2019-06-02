f670532a1d600394a88485b587227932f344361d
Jeff Parsons <parsoj@gmail.com>
2019-06-01 17:10:31 -0700

    return to last desktop when destroying a desktop

--- osx/scripts/destroy_workspace.sh
+++ osx/scripts/destroy_workspace.sh
@@ -2,6 +2,7 @@
 
 # query current focused desktop
 current_desktop_id=$(chunkc tiling::query --desktop id)
+prev_desktop_id=$(chunkc get _last_active_desktop)
 
 # clean up all windows on desktop
 window_query=$(chunkc tiling::query --windows-for-desktop $current_desktop_id | sed 's/ //g')
@@ -20,3 +21,6 @@ done <<< "$window_query"
 
 # destroy current desktop
 chunkc tiling::desktop --annihilate
+
+# move to prev desktop
+chunkc tiling:desktop --focus $prev_desktop_id
