9997410c79e24573ddaf13905bcdc0928a451941
Jeff Parsons <parsoj@gmail.com>
2019-08-12 09:22:25 -0700

    lsp module

---
+++ emacs/straight/modules/coding/language_features/lsp.el
@@ -0,0 +1,18 @@
+;;; lsp.el --- support for language servers          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Jeff Parsons
+
+;; Author: Jeff Parsons <jeffp@MacBook-Pro-9.local>
+;; Keywords: tools,
+
+(use-package lsp-mode
+  :commands lsp
+  )
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+(use-package company-lsp
+  :commands company-lsp)
+
+(use-package lsp-treemacs
+  :commands lsp-treemacs-errors-list)
