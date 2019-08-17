d28a567a5d5a032b5f01ed9ba4426d98a6123bfb
Jeff Parsons <jeff@Jeffs-MacBook-Air.local>
2019-08-17 15:00:11 -0700

    fix projectile project switching

--- emacs/straight/modules/projects/projectile.el
+++ emacs/straight/modules/projects/projectile.el
@@ -1,5 +1,11 @@
 (use-package projectile
   :init
-  (setq projectile-globally-ignored-files '("~" ".swp")) 
-
+  (setq
+   projectile-globally-ignored-files '("~" ".swp") 
+   projectile-project-search-path '("~/code/projects" "~/code/reference-code/")
+   projectile-project-root-files-functions '(projectile-root-top-down)
+   projectile-enable-caching 1
+   )
+  :config
+  (projectile-global-mode)
   )
