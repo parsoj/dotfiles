e09f53afe505040190eafe80fd793aa6e8d1be6e
Jeff Parsons <jeff@Jeffs-MacBook-Air.local>
2019-08-18 14:23:29 -0700

    fix doom code search paths

--- emacs/straight/modules/doom-stuff/doom_parse_helpers.el
+++ emacs/straight/modules/doom-stuff/doom_parse_helpers.el
@@ -2,6 +2,6 @@
 (defun get-doom-modules-list ()
   (append
    (directory-files-recursively "~/dotfiles/emacs/doom/modules" "\\.el$")
-   (directory-files-recursively "~/doom-emacs/modules" "[^#]\\.el$")
+   (directory-files-recursively "~/code/reference-code/doom-emacs/modules" "[^#]\\.el$")
    )
 )
