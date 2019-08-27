bec660173a984edf62455ec387ed53fe69406e41
Jeff Parsons <parsoj@gmail.com>
2019-08-26 17:28:43 -0700

    move eldoc to emacs-lisp module for now - add eldoc-box

--- emacs/modules/text_editing/coding/languages/emacs-lisp.el
+++ emacs/modules/text_editing/coding/languages/emacs-lisp.el
@@ -1,15 +1,24 @@
 
 
-(use-package elisp-slime-nav
-  :hook ((emcas-lisp-mode . elisp-slime-nav-mode)
-	 (emacs-lisp-mode . company-mode)
-	 (emacs-lisp-mode . rainbow-delimiters-mode)
-	 (emacs-lisp-mode . highlight-symbol-mode)
-	 (emacs-lisp-mode . flycheck-mode))
-)
+(use-package elisp-slime-nav 
+  :hook ((emcas-lisp-mode . elisp-slime-nav-mode) 
+	 (emacs-lisp-mode . company-mode) 
+	 (emacs-lisp-mode . rainbow-delimiters-mode) 
+	 (emacs-lisp-mode . highlight-symbol-mode) 
+	 (emacs-lisp-mode . flycheck-mode)))
 
 (use-package elisp-format)
 
-(use-package highlight-quoted
-  :hook ((emacs-lisp-mode . highlight-quoted-mode))
+(use-package 
+  highlight-quoted 
+  :hook ((emacs-lisp-mode . highlight-quoted-mode)))
+
+(use-package eldoc
+  :straight (eldoc :type built-in)
+  :init
+  (setq eldoc-documentation-function (lambda () (documentation (symbol-at-point))))
+)
+
+(use-package eldoc-box 
+  :hook (emacs-lisp-mode . eldoc-box-hover-mode)
 )
