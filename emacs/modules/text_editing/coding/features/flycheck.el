df8fee26fc1698b6be56719d70b14cabb44c482c
Jeff Parsons <parsoj@gmail.com>
2019-09-12 12:02:18 -0700

    add idle delay to flycheck mode for perf reasons

--- emacs/modules/text_editing/coding/features/flycheck.el
+++ emacs/modules/text_editing/coding/features/flycheck.el
@@ -1,4 +1,8 @@
-(use-package flycheck)
+(use-package flycheck
+:init 
+;; we need to delay/debounce flycheck runs - its too expensive to run with post-command-hook
+(setq flycheck-idle-change-delay 4) 
+  )
 
 ;(use-package flycheck-posframe
 ;  :after flycheck
