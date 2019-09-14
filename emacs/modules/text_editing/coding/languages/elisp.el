40b0e306dc1925672413739a235ad20341e0486f
Jeff Parsons <parsoj@gmail.com>
2019-09-14 08:59:57 -0700

    rename elisp config

---
+++ emacs/modules/text_editing/coding/languages/elisp.el
@@ -0,0 +1,25 @@
+
+
+(use-package elisp-slime-nav 
+  :hook ((emcas-lisp-mode . elisp-slime-nav-mode) ))
+
+(use-package elisp-format
+  :hook 
+  )
+
+(use-package 
+  highlight-quoted 
+  :hook ((emacs-lisp-mode . highlight-quoted-mode)))
+
+(use-package emacs-lisp-mode
+  :straight (:type built-in)
+  :hook
+    ((emacs-lisp-mode . company-mode) 
+    (emacs-lisp-mode . rainbow-delimiters-mode) 
+    (emacs-lisp-mode . highlight-symbol-mode) 
+    (emacs-lisp-mode . flycheck-mode)
+    (emacs-lisp . lispy-mode)
+    (ielm-mode . company-mode))
+
+  )
+  
