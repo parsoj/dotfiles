e2a56e3bfc4f8798a3ff6664ae6fa07a75de04eb
Jeff Parsons <parsoj@gmail.com>
2019-09-14 09:03:05 -0700

    initial swift setup

--- emacs/modules/text_editing/coding/languages/swift.el
+++ emacs/modules/text_editing/coding/languages/swift.el
@@ -1,8 +1,18 @@
+
+
 (use-package swift-mode
   :hook (swift-mode . (lambda () (lsp))))
 
 (use-package lsp-sourcekit
   :after lsp-mode
-  :config
-  (setenv "SOURCEKIT_TOOLCHAIN_PATH" "/Library/Developer/Toolchains/swift-latest.xctoolchain")
-  (setq lsp-sourcekit-executable (expand-file-name "<path_to_sourcekit-lsp_executable>")))
+  :init
+  (setq
+   sourcekit-toolchain-path "/Library/Developer/Toolchains/swift-5.1-DEVELOPMENT-SNAPSHOT-2019-08-31-a.xctoolchain"
+   sourcekit-exec-files (concat sourcekit-toolchain-path "/usr/bin")
+   lsp-sourcekit-executable (concat sourcekit-exec-files "sourcekit-lsp")
+   )
+
+  (setenv "SOURCEKIT_TOOLCHAIN_PATH" sourcekit-toolchain-path) 
+  (add-to-exec-path sourcekit-exec-files)
+  (setenv "TOOLCHAINS" "swift")
+  )
