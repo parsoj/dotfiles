2eee5cc83716c8cd0bd339638d2ba7eddc8e688d
Jeff Parsons <parsoj@gmail.com>
2019-10-30 07:36:50 -0700

    add some initial org todo keywords

---
+++ emacs/modules/org/org.el
@@ -0,0 +1,16 @@
+
+(setq org-todo-keyword-faces `(
+                               ("PROJECT" . ,(doom-color 'violet))
+                               ("TODO" . ,(doom-color 'yellow))
+                               ("NEXT" . ,(doom-color 'red))
+                               ("INBOX" . ,(doom-color 'yellow))
+                               ("LATER" . ,(doom-color 'teal))
+                               ("DONE" . ,(doom-color 'grey))
+                               ("CANCELLED" . ,(doom-color 'grey))
+                               ("GOAL" . ,(doom-color 'green))
+                               )
+
+      org-todo-keywords '(
+                          (sequence "TODO(t)" "INBOX(i)" "NEXT(n)" "IN-PROGRESS(p)" "LATER(l)" "WAITING(w)" "BLOCKED(b)" "|" "CANCELLED(c)" "DONE(d!)" )
+                          (sequence "PROJECT(p)" "|" "PROJECT-COMPLETED(P)")
+                          ))
