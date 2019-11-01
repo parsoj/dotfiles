a3eb3a7e5dd12309b33e3224a2c1e39c56668210
Jeff Parsons <parsoj@gmail.com>
2019-11-01 09:20:26 -0700

    add-to-exec-path should append at beginning

--- emacs/modules/core/env.el
+++ emacs/modules/core/env.el
@@ -2,7 +2,7 @@
 
 (defun add-to-exec-path (&rest items)
   (progn
-    (setq exec-path (append exec-path items ) ) 
+    (setq exec-path (append items exec-path ) ) 
     (setenv "PATH" (string-join exec-path ":") )))
 
 (add-to-exec-path "/usr/local/opt/bin") 
