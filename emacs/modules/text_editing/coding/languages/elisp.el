816f72093ceb9c196023855ae0ab891cd1b9ea61
Jeff Parsons <parsoj@gmail.com>
2019-11-05 11:33:17 -0800

    add hasklig-mode hook for elisp

--- emacs/modules/text_editing/coding/languages/elisp.el
+++ emacs/modules/text_editing/coding/languages/elisp.el
@@ -24,6 +24,7 @@
     (emacs-lisp-mode . rainbow-delimiters-mode) 
     (emacs-lisp-mode . highlight-symbol-mode) 
     (emacs-lisp-mode . flycheck-mode)
+    (emacs-lisp-mode . hasklig-mode)
     (emacs-lisp . lispy-mode)
     (ielm-mode . company-mode))
 
