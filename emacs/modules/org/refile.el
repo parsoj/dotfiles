019a75c07f71bd137a160c34d364a0b817b495e4
Jeff Parsons <parsoj@gmail.com>
2019-11-05 22:22:23 -0800

    format refile config

--- emacs/modules/org/refile.el
+++ emacs/modules/org/refile.el
@@ -1,19 +1,20 @@
 
-(setq org-refile-use-outline-path 'file
-      org-outline-path-complete-in-steps nil)
+(with-eval-after-load '+org-core
+
+  (setq org-refile-use-outline-path 'file org-outline-path-complete-in-steps nil)
 
 
-(setq org-root "~/org") 
-(setq org-refile-dirs '("projects" "on_hold" "reference" "goals_and_aspirations"))
+  (setq org-refile-dirs '("projects" "on_hold" "reference" "goals_and_aspirations"))
 
 
-(defun refresh-org-refile-targets ()
-  (interactive)
-  (setq org-refile-targets `(
-			   (,(mapcan
-			     (lambda (dir) (directory-files-recursively (concat org-root "/" dir) "\\.org$"))
-			     org-refile-dirs) .
-			   (:maxlevel . 3))))
+  (defun refresh-org-refile-targets () 
+    (interactive) 
+    (setq org-refile-targets `((,(mapcan (lambda (dir) 
+					   (directory-files-recursively
+					    (concat org-root "/" dir)
+					    "\\.org$")) org-refile-dirs)
+				. 
+				(:maxlevel . 3)))))
+
+  (refresh-org-refile-targets)
 )
-
-(refresh-org-refile-targets)
