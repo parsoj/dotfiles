314598b43ac6fdd3dc393abd38337c6b1c07509a
Jeff Parsons <parsoj@gmail.com>
2019-11-21 08:44:13 -0800

    fixed elisp-mode hooks

--- emacs/modules/editor/languages/elisp.el
+++ emacs/modules/editor/languages/elisp.el
@@ -2,7 +2,7 @@
 
 (use-package 
   elisp-slime-nav 
-  :hook ((emacs-lisp-mode . elisp-slime-nav-mode) ) 
+  ;;:hook ((emacs-lisp-mode . elisp-slime-nav-mode) ) 
   :config (setq +nav-goto-def-func (lambda () 
 				     (elisp-slime-nav-find-elisp-thing-at-point (symbol-name
 										 (symbol-at-point))))) 
@@ -13,31 +13,21 @@
 (use-package 
   elisp-format)
 
-(use-package 
-  highlight-quoted 
-  :hook emacs-lisp-mode)
 
-
-(straight-use-package '(emacs-lisp-mode
-  :type built-in
-  :config
-  (general-add-hook 'emacs-lisp-mode-hook
-		    (list
-		     #'company-mode
-		     #'rainbow-delimiters-mode
-		     #'highlight-symbol-mode
-		     #'flycheck-mode
-		     #'hasklig-mode
-		     #'lisp-butt-mode 
-		     #'lispy-mode 
-		     ))))
+(with-eval-after-load 'general
+(general-add-hook 
+ 'emacs-lisp-mode-hook
+ (list
+  #'lispy-mode
+  #'company-mode
+  #'lisp-butt-mode
+  #'rainbow-delimiters-mode
+  #'highlight-quoted-mode
+  #'hasklig-mode
+  ))
+)
    
 
-
-
 (setq +doc-at-point-func 'describe-function-in-popup)
-
-
-
 
 (provide '+elisp)
