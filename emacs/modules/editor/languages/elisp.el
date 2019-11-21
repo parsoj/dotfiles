68cfb1bfbb9808f78a8b25cc5e233775001a920c
Jeff Parsons <parsoj@gmail.com>
2019-11-20 19:14:41 -0800

    elisp built-in package config, and hooks config working

--- emacs/modules/editor/languages/elisp.el
+++ emacs/modules/editor/languages/elisp.el
@@ -15,22 +15,29 @@
 
 (use-package 
   highlight-quoted 
-  :hook ((emacs-lisp-mode . highlight-quoted-mode)))
+  :hook emacs-lisp-mode)
 
 
-(with-eval-after-load 'general
-  (general-add-hook emacs-lisp-mode-hook
-		    (list #'company-mode
-			  #'rainbow-delimiters-mode
-			  #'highlight-symbol-mode
-			  #'flycheck-mode
-			  #'hasklig-mode
-			  #'lisp-butt-mode
-			  (lambda () (lispy-mode 1))))
+(straight-use-package '(emacs-lisp-mode
+  :type built-in
+  :config
+  (general-add-hook 'emacs-lisp-mode-hook
+		    (list
+		     #'company-mode
+		     #'rainbow-delimiters-mode
+		     #'highlight-symbol-mode
+		     #'flycheck-mode
+		     #'hasklig-mode
+		     #'lisp-butt-mode 
+		     #'lispy-mode 
+		     ))))
+   
 
 
-  )
 
-(setq +doc-at-point-func 'describe-function-in-popup) 
+(setq +doc-at-point-func 'describe-function-in-popup)
+
+
+
 
 (provide '+elisp)
