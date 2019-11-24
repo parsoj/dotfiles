605235190f6f626fea394726690b775bcf7f0995
Jeff Parsons <parsoj@gmail.com>
2019-11-24 15:59:34 -0800

    add configs for cleverparens

--- emacs/modules/editor/coding_features/lisp/cleverparens.el
+++ emacs/modules/editor/coding_features/lisp/cleverparens.el
@@ -4,6 +4,12 @@
   (require 'smartparens-config)
 
   )
-(use-package evil-cleverparens)
+(use-package evil-cleverparens
+  :config
+  (setq evil-cleverparens-use-additional-movement-keys t) 
+  (setq evil-mode-beyond-eol t) 
+  )
 
+
+(use-package aggressive-indent)
 
