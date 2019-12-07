5f4dc926ef08afec82e67aa173816864f9f49078
Jeff Parsons <parsoj@gmail.com>
2019-12-07 12:06:09 -0800

    clearer comment for (org-reload)

--- emacs/modules/org/org.el
+++ emacs/modules/org/org.el
@@ -4,6 +4,7 @@
   :config
   ;; running (org-reload) forces emacs to load the newest version of org,
   ;; rather than just sticking with the built-in version
+  ;;(we have deps on the newest version of org, so we need to do this)
   (org-reload)
 
   (with-eval-after-load 'doom-themes
