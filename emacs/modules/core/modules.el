8ade488560c8b642b1b4cf02a9c00301718eee2e
Jeff Parsons <parsoj@gmail.com>
2019-12-07 13:04:00 -0800

    some auto-formatting

--- emacs/modules/core/modules.el
+++ emacs/modules/core/modules.el
@@ -47,16 +47,16 @@
 (defun create-new-module ()
   (interactive)
   (let ((module-dir (completing-read
-		      "Pick Directory to create in: "
-		      (get-modules-directories)
-		      nil t)))
+		     "Pick Directory to create in: "
+		     (get-modules-directories)
+		     nil t)))
 
     (counsel-file-jump nil module-dir)
     ;; TODO load list of directories, and prompt for parent dir for new module
     ;; TODO ask for file/path name from that starting point
     ;; TODO create new file there and open it
     )
-)
+  )
 
 (defun create-new-scratch-file ()
   (interactive)
