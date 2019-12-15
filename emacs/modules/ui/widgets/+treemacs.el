9525c0aba132b42b626f7285f0466ffd90908a39
Jeff Parsons <parsoj@gmail.com>
2019-12-15 09:42:20 -0800

    working func for "add project to treemacs"

--- emacs/modules/ui/widgets/+treemacs.el
+++ emacs/modules/ui/widgets/+treemacs.el
@@ -1,8 +1,10 @@
 ;;; +treemacs.el -*- lexical-binding: t; -*-
 
+
 (use-package treemacs
   :config
-  (treemacs-follow-mode nil) 
+  (treemacs-follow-mode t) 
+  (treemacs-filewatch-mode t)
 
   (setq treemacs-width 35
         treemacs-display-in-side-window t
@@ -14,6 +16,12 @@
                                   (fringe-mode 0)
                                   ;;(setq buffer-face-mode-face `(:background "#211C1C"))
                                   (buffer-face-mode 1)))
+
+
+
+
+
+  
   )
 
 (use-package treemacs-evil
@@ -21,15 +29,48 @@
   )
 
 (use-package treemacs-projectile
-  :after treemacs
+  :after treemacs projectile
   )
 
 (use-package treemacs-magit
   :after treemacs magit
   )
 
+(defun +treemacs-find-workspace-for-path (path) 
+  (--first (treemacs-is-path path :in-workspace it)
+           (treemacs-workspaces)))
 
+(defun +treemacs-open-current-project ()
+  (interactive)
 
+  (let* ((path (projectile-ensure-project (projectile-project-root)))
+         (name (projectile-project-name))
+	 (found-treemacs-workspace (+treemacs-find-workspace-for-path path))
+	 )
+    (progn
+      (treemacs-select-window) 
+
+      ;; if the project exists in a workspace already - open that workspace
+      ;; otherwise, jump to the default workspace, and add the project there
+      (if  found-treemacs-workspace  
+	  (setf (treemacs-current-workspace)
+		found-treemacs-workspace)
+	(progn
+	  (setf (treemacs-current-workspace)
+		(car ( treemacs-workspaces))
+		)
+	  (treemacs-do-add-project-to-workspace path name)
+	  )
+	
+	)
+
+      (treemacs--rerender-after-workspace-change)
+      )))
+
+(defun +treemacs-close-window ()
+  (interactive)
+  (if (eq (treemacs-current-visibility) 'visible)
+      (delete-window (treemacs-get-local-window))))
 
 (provide '+treemacs)
 ;;; +treemacs.el ends here
--- emacs/modules/ui/widgets/treemacs-fixes.dev.el
+++ emacs/modules/ui/widgets/treemacs-fixes.dev.el
@@ -1,14 +1,6 @@
 
-(defun +treemacs-find-and-switch-workspace ()
-  (interactive)
-  (progn
-    (treemacs--find-workspace) 
-
-    (treemacs--rerender-after-workspace-change)
-
-    )
-
-  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; scratch code
 
 (defun spacemacs/treemacs-project-toggle ()
   "Toggle and add the current project to treemacs if not already added."
@@ -22,43 +14,53 @@
       (treemacs-do-add-project-to-workspace path name)
       (treemacs-select-window))))
 
+(treemacs-current-workspace)
 
-(defun open-current-project-in-treemacs ()
+(setq found-treemacs-workspace (+treemacs-find-workspace-for-path (buffer-file-name)))
+
+(treemacs-workspaces)
+(#s(treemacs-workspace "Default" (#s(treemacs-project "digcore-store-srvc" "/Users/jeffparsons/lulu/digcore-store-srvc" local-readable) #s(treemacs-project "organice" "/Users/jeffparsons/code/organice" local-readable) #s(treemacs-project "org" "/Users/jeffparsons/Dropbox/org" local-readable) #s(treemacs-project "configuration" "/Users/jeffparsons/configuration" local-readable))) #s(treemacs-workspace "configuration" (#s(treemacs-project "configuration" "/Users/jeffparsons/configuration" local-readable))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; utility funcs
+(defun +treemacs-find-workspace-for-path (path) 
+  (--first (treemacs-is-path path :in-workspace it)
+           (treemacs-workspaces)))
+
+(defun +treemacs-open-current-project ()
   (interactive)
 
-  (let ((path (projectile-ensure-project (projectile-project-root)))
-        (name (projectile-project-name))
-	)
+  (let* ((path (projectile-ensure-project (projectile-project-root)))
+         (name (projectile-project-name))
+	 (found-treemacs-workspace (+treemacs-find-workspace-for-path path))
+	 )
     (progn
       (treemacs-select-window) 
 
-      (if ( treemacs-find-workspace-for-path path) 
-
-	  (setf (treemacs-current-workspace) 
-		(treemacs-find-workspace-for-path path))
-
+      ;; if the project exists in a workspace already - open that workspace
+      ;; otherwise, jump to the default workspace, and add the project there
+      (if  found-treemacs-workspace  
+	  (setf (treemacs-current-workspace)
+		found-treemacs-workspace)
+	(progn
+	  (setf (treemacs-current-workspace)
+		(car ( treemacs-workspaces))
+		)
+	  (treemacs-do-add-project-to-workspace path name)
+	  )
+	
 	)
 
-      ;;(treemacs-do-add-project-to-workspace path name)
       (treemacs--rerender-after-workspace-change)
 
-      )
-    )
+      )))
 
-  )
-
-(defun treemacs-find-workspace-for-path (path) 
-  (--first (treemacs-is-path path :in-workspace it)
-           (treemacs-workspaces)  
-	   )
-  )
+(defun +treemacs-close-window ()
+  (interactive)
+  (if (eq (treemacs-current-visibility) 'visible)
+      (delete-window (treemacs-get-local-window))))
 
 
-
-
-(setf (treemacs-current-workspace)
-      (let ((it (treemacs-current-workspace))
-	    (path default-directory))
-	(--first (treemacs-is-path path :in-workspace it)
-		 treemacs--workspaces)))
 
