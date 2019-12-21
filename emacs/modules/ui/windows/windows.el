8aefa01f2aae0da63161e19a9eb9ca8ae48e7b0a
Jeff Parsons <parsoj@gmail.com>
2019-12-21 08:45:19 -0800

    add initial window move keybinds

--- emacs/modules/ui/windows/windows.el
+++ emacs/modules/ui/windows/windows.el
@@ -2,5 +2,20 @@
 (use-package winum
   :config
   (winum-mode)
-)
+
+  (general-define-key
+   :states '(normal visual emacs movement treemacs)
+   :prefix "SPC"
+   "0" 'winum-select-window-0
+   "1" 'winum-select-window-1
+   "2" 'winum-select-window-2
+   "3" 'winum-select-window-3
+   "4" 'winum-select-window-4
+   "5" 'winum-select-window-5
+   "6" 'winum-select-window-6
+   "7" 'winum-select-window-7
+   "8" 'winum-select-window-8
+   "9" 'winum-select-window-9
+   )
+  )
 
