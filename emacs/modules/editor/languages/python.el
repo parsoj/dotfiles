b0bbe69c7234c82b1e4a3af0ed2cd46cd52512f0
Jeff Parsons <parsoj@gmail.com>
2019-12-21 09:07:11 -0800

    just use python-lsp for now

--- emacs/modules/editor/languages/python.el
+++ emacs/modules/editor/languages/python.el
@@ -1,37 +1,42 @@
 (use-package python
   )
 
-(use-package anaconda-mode
-  :hook ((python-mode . anaconda-mode)
-	 (python-mode . anaconda-eldoc-mode))
+;;(use-package anaconda-mode
+;;  :hook ((python-mode . anaconda-mode)
+;;	 (python-mode . anaconda-eldoc-mode))
+;;  )
+;;
+;;(use-package company-anaconda
+;;  :after anaconda-mode company
+;;  )
+
+;;(use-package pip-requirements
+;;  :hook (pip-requirements-mode . pip-requirements-auto-complete-setup)
+;;  )
+
+
+
+;;(use-package py-isort)
+
+
+;;(use-package yapfify
+;;  :after python
+;;  :hook (python-mode . yapf-mode)
+;;  )
+
+
+;;(use-package pylookup)
+
+;;(with-eval-after-load 'general
+;;  (general-add-hook 
+;;   'python-mode-hook
+;;   (list
+;;    #'lsp-deferred
+;;    #'flycheck-mode
+;;    ))
+;;  )
+
+
+(use-package lsp-python-ms
+  :hook (python-mode . lsp-deferred)
   )
-
-(use-package company-anaconda
-  :after anaconda-mode company
-  )
-
-(use-package pip-requirements
-  :hook (pip-requirements-mode . pip-requirements-auto-complete-setup)
-  )
-
-
-(with-eval-after-load 'general
-  (general-add-hook 
-   'python-mode-hook
-   (list
-    #'lsp-deferred
-    #'lsp-ui
-    #'flycheck-mode
-    ))
-  )
-
-(use-package py-isort)
-
-
-(use-package yapfify
-  :after python
-  :hook (python-mode . yapf-mode)
-  )
-
-
-(use-package pylookup)
