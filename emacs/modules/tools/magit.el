714a30d50d3a7cddae65a73070df354de0a1fe44
Jeff Parsons <parsoj@gmail.com>
2019-12-21 08:57:33 -0800

    fix magit buffer popup

--- emacs/modules/tools/magit.el
+++ emacs/modules/tools/magit.el
@@ -9,6 +9,6 @@
 (add-to-list 'display-buffer-alist
 	     `("magit:"
 	       (display-buffer-in-side-window)
-	       (side . top)
-	       (window-width . 0.5)
+	       (side . right)
+	       (window-width . 100)
 	       ))
