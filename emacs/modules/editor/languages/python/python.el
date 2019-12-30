69da2c594f497d8b001981658eaaec71b254d969
Jeff Parsons <parsoj@gmail.com>
2019-12-30 14:07:21 -0800

    python lsp working (no unresolved imports)

--- emacs/modules/editor/languages/python/python.el
+++ emacs/modules/editor/languages/python/python.el
@@ -1,33 +1,16 @@
+(setq python-bin "/usr/local/bin/python3")
 (use-package python
   :init
-  (setq python-shell-interpreter "python3")
+  (setq python-shell-interpreter python-bin)
   )
 
-;;(use-package pip-requirements
-;;  :hook (pip-requirements-mode . pip-requirements-auto-complete-setup)
-;;  )
-
-
-;;(use-package py-isort)
-
-;;(use-package yapfify
-;;  :after python
-;;  :hook (python-mode . yapf-mode)
-;;  )
-
-;;(use-package pylookup)
-
-;;(with-eval-after-load 'general
-;;  (general-add-hook 
-;;   'python-mode-hook
-;;   (list
-;;    #'lsp-deferred
-;;    #'flycheck-mode
-;;    ))
-;;  )
 
 (use-package lsp-python-ms
+  :init
+  (setq  lsp-python-ms-python-executable-cmd python-bin)
   )
+
+(use-package virtualenvwrapper)
 
 (with-eval-after-load 'general
   (general-add-hook 
@@ -46,7 +29,8 @@
   )
 
 (with-eval-after-load 'dap-mode
-  (setq dap-python-executable "python3")
+  (setq dap-python-executable python-bin)
+
   (require 'dap-python)
 
 
@@ -68,3 +52,18 @@
 	       (display-buffer-at-bottom)
 	       (window-height . 20)
 	       ))
+
+
+;;(use-package pip-requirements
+;;  :hook (pip-requirements-mode . pip-requirements-auto-complete-setup)
+;;  )
+
+
+;;(use-package py-isort)
+
+;;(use-package yapfify
+;;  :after python
+;;  :hook (python-mode . yapf-mode)
+;;  )
+
+;;(use-package pylookup)
