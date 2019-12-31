d042159876e95a585fc36288a94dd1a09e1c3158
Jeff Parsons <parsoj@gmail.com>
2019-12-30 18:14:22 -0800

    add fix for ob-restclient async

--- emacs/modules/editor/languages/org/babel/restclient.el
+++ emacs/modules/editor/languages/org/babel/restclient.el
@@ -1,2 +1,10 @@
 (use-package ob-restclient
+  :config
+  
+  (with-eval-after-load 'ob-async
+    ;; async execution happens in a sub-process that doesn't have the default init.el loaded - this hook makes sure that the ob-restclient code is available in that context
+    ;; XXXXX- eventually, this may become problemmatic if there are a ton of things running in this hook every time
+    (add-hook 'ob-async-pre-execute-src-block-hook
+              '(lambda ()
+		 (require 'ob-restclient))))
   )
