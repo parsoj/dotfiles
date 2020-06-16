49efd3b2c8917e6c4ca1e7ba7afc1a8ec9fe93ec
Jeff Parsons <parsoj@gmail.com>
2020-06-16 15:40:23 -0700

    move config to zsh

--- bash/apply.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ~/.bashrc ; ln -s $(pwd)/bashrc ~/.bashrc
-rm -rf ~/.bash_profile ; ln -s $(pwd)/bash_profile ~/.bash_profile
---
+++ zsh/apply.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.zshrc ; ln -s $(pwd)/zshrc ~/.zshrc
+
+#rm -rf ~/.bashrc ; ln -s $(pwd)/bashrc ~/.bashrc
+#rm -rf ~/.bash_profile ; ln -s $(pwd)/bash_profile ~/.bash_profile
