dcb4c765d4248509d27bfe1171416b5ff023b282
Jeff Parsons <parsoj@gmail.com>
2020-10-02 13:40:26 -0700

    doom bootstrapping configs

---
+++ emacs/setup-doom.sh
@@ -0,0 +1,7 @@
+EMACS_CONF_ROOT=~/.config/emacs
+
+rm -rf ~/.emacs.d ; ln -s $EMACS_CONF_ROOT/doom-core ~/.emacs.d
+rm -rf ~/.doom.d; ln -s $EMACS_CONF_ROOT/doom-config ~/.doom.d
+
+export PATH=$PATH:~/.emacs.d/bin
+doom install
---
+++ osx/install_fonts.sh
@@ -0,0 +1,3 @@
+brew tap homebrew/cask-fonts
+brew cask install font-iosevka
+brew cask install font-iosevka-slab
