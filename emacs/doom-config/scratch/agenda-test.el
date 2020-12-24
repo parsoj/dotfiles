ad850f8efab364f9f1db735215add1616915ac08
Jeff Parsons <parsoj@gmail.com>
2020-12-24 14:34:37 -0800

    "available today" section using org-ql

--- emacs/doom-config/scratch/agenda-test.el
+++ emacs/doom-config/scratch/agenda-test.el
@@ -1,1 +1,10 @@
 ;;; ../.config/emacs/doom-config/scratch/agenda-test.el -*- lexical-binding: t; -*-
+
+
+(setq org-active-states '("NEXT" "AVAILABLE"))
+
+;;  "Available today" section
+(org-ql-search org-agenda-files `(and (tags "active")
+                                      (or (scheduled :to today)
+                                          (not (scheduled)))
+                                      ,(append '(todo) org-active-states)))
