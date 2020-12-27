234e13ab7078772c2dbae5dfb181b76cfa6b8eba
Jeff Parsons <parsoj@gmail.com>
2020-12-27 14:26:50 -0800

    add org-ql agenda block

--- emacs/doom-config/extras/org/agenda.el
+++ emacs/doom-config/extras/org/agenda.el
@@ -22,6 +22,7 @@
 
 (setq org-agenda-files (org-get-agenda-files))
 
+
 ;; (defun org-set-agenda-files ()
 ;;   (interactive)
 ;;   (setq org-agenda-files (org-get-agenda-files))
@@ -38,56 +39,30 @@
 (use-package! org-super-agenda)
 (use-package! org-ql)
 
-(org-super-agenda-mode 1)
+;; (org-super-agenda-mode 1)
+
+(setq org-active-states '("NEXT" "AVAILABLE"))
 
 (progn
-
-  ;; (setq org-super-agenda-groups '(
-  ;;                                 ;; (:name "Due today"
-  ;;                                 ;;  :deadline today)
-
-  ;;                                 ;; (:name "Today"
-  ;;       			  ;;  ;; :time-grid t
-  ;;       			  ;;  :scheduled today)
-  ;;                                 (:name "Work Items"
-  ;;                                  :and (
-  ;;                                        :todo "INBOX"
-  ;;                                        :scheduled (past today)
-  ;;                                        )
-  ;;                                  )
-
-  ;;                                 (:name "Currently Active Projects"
-  ;;                                  :and (
-  ;;                                        :todo "PROJECT"
-
-  ;;                                        )
-  ;;                                  )
-
-  ;;                                 ))
 
 (setq org-agenda-custom-commands '(
                                    ("x" "custom agenda"
                                     (
-                                     (tags "+TODO=\"INBOX\""
-                                           ((org-agenda-overriding-header "Inbox Items")))
-                                     (tags "active/!NEXT"
-                                           ((org-agenda-overriding-header "Next items")))
-                                     (tags "active/!AVAILABLE|NEXT"
-                                           ((org-agenda-overriding-header "On Deck")))
+                                      (tags "+TODO=\"INBOX\""
+                                            ((org-agenda-overriding-header "Inbox Items")))
+                                      (org-ql-block `(and (tags "active")
+                                                          (or (scheduled :to today)
+                                                              (not (scheduled)))
+                                                          ,(append '(todo) org-active-states))
+                                                    ((org-ql-block-header "Due Today")))
+                                     (tags "SCHEDULED<=\"<today>\"&+active"
+                                           ((org-agenda-overriding-header "Available Today")
+                                            ))
                                      (tags "TODO=\"PROJECT\"&+active"
                                            ((org-agenda-overriding-header "Active Projects")
                                             (org-super-agenda-groups '((:auto-category t)))
                                             ))
-                                     (tags "SCHEDULED<=\"<today>\"&+active"
-                                           ((org-agenda-overriding-header "Items For Today")
-                                            ))
-                                     ;; (agenda "" ((org-agenda-span 1)
-                                     ;;             (org-agenda-start-day "today")))
-                                     ;; (todo "NEXT")
-                                     ;; ;; (todo "INBOX")
-                                     ;; (todo "PROJECT")
-                                     ;;
                                      ))))
 
-  (org-agenda t "x")
-  )
+(org-agenda t "x")
+)
