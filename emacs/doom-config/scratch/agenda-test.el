d22ef35a1228fb1ba59e7f2905aaa8c3a7178052
Jeff Parsons <parsoj@gmail.com>
2021-02-17 12:45:26 -0800

    add skip function for block agenda

--- emacs/doom-config/scratch/agenda-test.el
+++ emacs/doom-config/scratch/agenda-test.el
@@ -7,47 +7,31 @@
 
      ;; (org-agenda-files '("~/.config/emacs/doom-config/scratch/test.org" "~/.config/emacs/doom-config/scratch/foobar.org"))
      (org-agenda-custom-commands '(
-                                   ("m" "Morning Planning"
+                                   ("d" "Daily Planning"
                                     (
-                                     (tags "+TODO=\"INBOX\""
-                                           ((org-agenda-overriding-header "Inbox Items")))
-                                     (org-ql-block `(and
-                                                     ,(append '(todo) org-active-states)
-                                                     (deadline :to today)
-                                                     )
-                                                   ((org-ql-block-header "Due Today")))
-                                     (agenda ""
-                                             ((org-agenda-start-day "1d")
-                                              (org-agenda-span 1))
-
-                                             )
-                                     (org-ql-block
-                                      `(and
-                                        (todo "PROJECT")
-                                        (tags "active")
-                                        )
-                                      ((org-ql-block-header "Active Projects")
-                                       ;; (org-agenda-super-groups '((:auto-category t)))
-                                       (org-super-agenda-groups '((:auto-category t))))
-                                      )
-
+                                     (
+                                      ;; tags-todo "+TODO=\"TODO\""
+                                      agenda ""
+                                      ((org-agenda-overriding-header "Weekly Overview")
+                                       (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("ROUTINE")))
+                                       (org-agenda-span 5))) ;TODO
+                                     (tags "FOO"
+                                           ((org-agenda-overriding-header "Due Today"))) ;TODO
+                                     (tags "FOO"
+                                           ((org-agenda-overriding-header "Scheduled Today"))) ;TODO
+                                     (tags "FOO"
+                                           ((org-agenda-overriding-header "Due in new 3 days"))) ;TODO
+                                     (tags "FOO"
+                                           ((org-agenda-overriding-header "Next items for each project"))) ;TODO
+                                     (tags "FOO"
+                                           ((org-agenda-overriding-header "Project Activity"))) ;;TODO
                                      )
                                     )
-                                   ("w" "Weekly Planning"
-                                    (
-                                     (tags "+TODO=\"INBOX\""
-                                           ((org-agenda-overriding-header "Inbox Items")))
 
-                                     (agenda ""
-                                             ((org-agenda-start-day "1d")
-                                              (org-agenda-span 7))) ;;TODO filter out routine items from this view
-                                     )
-
-                                    )
                                    )))
 
 
   (org-super-agenda-mode 1)
   ;; (org-agenda t "w")
-  (org-agenda t "m")
+  (org-agenda t "d")
   )
