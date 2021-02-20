5a1ab44067339902fea3e892df62c9ebaad0df59
Jeff Parsons <parsoj@gmail.com>
2021-02-20 14:44:32 -0800

    add projectile config

---
+++ emacs/doom-config/extras/workspaces/projectile_config.el
@@ -0,0 +1,19 @@
+;;; projectile_config.el --- configurations for projectile  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Jeff Parsons
+
+;; Author: Jeff Parsons <parsoj@gmail.com>
+
+
+(after! projectile
+  ;;(setq projectile-project-root-functions '(projectile-root-bottom-up))
+  (setq projectile-project-root-files-bottom-up '(".projectile"))
+
+  )
+
+
+
+
+
+(provide 'projectile_config)
+;;; projectile_config.el ends here
