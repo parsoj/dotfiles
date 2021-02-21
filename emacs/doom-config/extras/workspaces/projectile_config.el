3f592125668a36ee58ae787f37144c4f935b78bc
Jeff Parsons <parsoj@gmail.com>
2021-02-20 18:35:08 -0800

    add a quick workspace creation func

--- emacs/doom-config/extras/workspaces/projectile_config.el
+++ emacs/doom-config/extras/workspaces/projectile_config.el
@@ -13,6 +13,20 @@
 
 
 
+(defun +create-new-workspace-at (parent-dir workspace-name)
+  (let ((workspace-path (concat parent-dir "/" workspace-name)))
+    (if (file-directory-p workspace-path)
+        t
+      (progn
+        (make-directory workspace-path t)
+        (write-region "" nil (concat workspace-path "/.projectile"))
+        (write-region "" nil (concat workspace-path "/" workspace-name ".org"))
+        )
+      ))
+  )
+
+
+
 
 
 (provide 'projectile_config)
