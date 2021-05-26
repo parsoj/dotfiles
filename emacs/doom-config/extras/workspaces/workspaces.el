d3bc4f1e9265f34fee4758ab89caa1cad043465c
Jeff Parsons <jeffparson@Jeffs-MacBook-Pro.local>
2021-05-26 16:26:53 -0700

    clean up workspaces code

--- emacs/doom-config/extras/workspaces/workspaces.el
+++ emacs/doom-config/extras/workspaces/workspaces.el
@@ -6,21 +6,24 @@
 
 (defun +create-new-workspace ()
   (interactive)
-  (let (
+  (let* (
         (workspace-parent-dir (completing-read "Select Workspace Dir: " (+list-workspace-directories workspaces-root)))
         (workspace-name (completing-read "Name for Workspace: " nil nil nil))
+        (workspace-path (concat workspace-parent-dir "/" workspace-name))
         )
     (progn
-      (+workspace-create-at workspace-parent-dir workspace-name)
+      (+workspace-create-at workspace-name workspace-path)
       (find-file
-       (concat workspaces-root "/" workspace-name "/" workspace-name ".org"))
+       (concat workspace-path "/" "notes.org"))
       )
     )
   )
 
+
 (defun +f-entries-names (dir)
   (--map (f-filename it) (f-entries dir))
   )
+
 
 (defun +list-workspace-directories (root-dir)
 
@@ -29,17 +32,19 @@
   )
 
 
-(defun +workspace-create-at (parent-dir workspace-name)
-  (let ((workspace-path (concat parent-dir "/" workspace-name)))
-    (if (file-directory-p workspace-path)
-        t
-      (progn
-        (make-directory workspace-path t)
-        (write-region "" nil (concat workspace-path "/.projectile"))
+(defun +workspace-create-at (workspace-name workspace-path)
+  (if (f-exists? workspace-path)
+      (throw 'directory-exists "There is already a file/directory at the workspace path given")
+    (progn
+      (make-directory workspace-path t)
+      (let ((default-directory workspace-path))
+        (write-region "" nil ".projectile")
+        (write-region (concat "#+TITLE: " workspace-name) nil "notes.org")
+        (magit-init default-directory)
         )
-      ))
+      )
+      )
   )
-
 
 ;; (defun +treemacs-workspace-switch-create (ws-name)
 ;;   (unless (eq 'success (car (treemacs-do-switch-workspace ws-name)))
