56e63a8039adc21c3269a41a874387d3f113d415
Jeff Parsons <parsoj@gmail.com>
2021-05-28 18:09:02 -0700

    auto-discover projectile projects

--- emacs/doom-config/extras/workspaces/projectile_config.el
+++ emacs/doom-config/extras/workspaces/projectile_config.el
@@ -20,9 +20,44 @@
 
   )
 
+
+
+;; (defcustom projectile-auto-discover-search-depth nil
+;;   "How deeply to recurse directories searching for projects.
+;; Must be a positive integer.
+;; When nil or <= 1, recursion is effectively disabled and only immediate subdirs
+;; are searched"
+;;   :group 'projectile
+;;   :type '(restricted-sexp
+;;           :match-alternatives ((lambda (x) (and (natnump x)  (not (zerop x))))))
+;;   :package-version '(projectile . "2.3.0"))
+
+
+(defun jeff/projectile-discover-projects-in-directory (directory &optional recursion-depth)
+  "Discover any projects in DIRECTORY and add them to the projectile cache.
+This function is not recursive and only adds projects with roots
+at the top level of DIRECTORY."
+  (interactive
+   (list (read-directory-name "Starting directory: ")))
+  (if (file-exists-p directory)
+      (let ((subdirs (directory-files directory t)))
+        (mapcar
+         (lambda (dir)
+           (when (and (file-directory-p dir)
+                      (not (member (file-name-nondirectory dir) '(".." "."))))
+             (if (projectile-project-p dir)
+                 (projectile-add-known-project dir)
+               (when (> recursion-depth 1)
+                 (projectile-discover-projects-in-directory dir (- recursion-depth 1))
+                 )
+               )))
+         subdirs))
+    (message "Project search path directory %s doesn't exist" directory)))
+
+
 (after! projectile
   ;;(setq projectile-project-root-functions '(projectile-root-bottom-up))
-  (setq projectile-project-root-files-bottom-up '(".projectile"))
+  ;; (setq projectile-project-root-files-bottom-up '(".projectile"))
 
   (set-popup-rule! "^\\*compilation\\*"  :side 'right :quit nil :select t)
 
@@ -32,10 +67,19 @@
 
   (map! :leader "p n"  (cmd! (find-file (concat (projectile-project-root) "notes.org"))))
 
-  (setq projectile-project-search-path '("~/workspaces"))
+  (setq projectile-project-search-path '("~/workspaces/"))
 
-  (setq projectile-project-root-files-bottom-up '(".projectile"))
-  (setq projectile-project-root-files-functions '(projectile-root-bottom-up ))
+
+  ;; (setq projectile-project-root-files-bottom-up '(".projectile"))
+  (setq projectile-project-root-files '(".projectile"))
+  (setq projectile-auto-discover t)
+
+
+  (jeff/projectile-discover-projects-in-directory "~/workspaces" 15)
+
+  ;; (setq projectile-project-root-files-top-down-recurring '(".projectile"))
+
+  ;; (setq projectile-project-root-functions '(projectile-root-top-down-recurring ))
 
 
   )
