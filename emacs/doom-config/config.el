b042335d57369b19e18512db8831f4e995e5b949
jeff <jeff@jeffs-MacBook-Pro.local>
2021-09-10 13:38:05 -0700

    add winow quick-switch functionality

--- emacs/doom-config/config.el
+++ emacs/doom-config/config.el
@@ -155,6 +155,18 @@
  :nvm "k" #'evil-next-line
       )
 
+(map! :leader
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9
+      )
+
 (after! treemacs-evil
   (evil-define-key 'treemacs treemacs-mode-map "k" #'evil-next-line)
   (evil-define-key 'treemacs treemacs-mode-map "l" #'evil-previous-line)
