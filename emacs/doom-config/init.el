8258ebc523d30835a6b64d2a798ffcbc6c66d4a4
Jeff Parsons <parsoj@gmail.com>
2021-11-04 12:07:19 -0700

    remove vertico and emoji packages for now

--- emacs/doom-config/init.el
+++ emacs/doom-config/init.el
@@ -49,15 +49,15 @@
             +precient
             )
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       ;; vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom                             ; what makes DOOM look the way it does
        ;; doom-dashboard                   ; a nifty splash screen for Emacs
-
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       ;; (emoji +unicode)
+                                        ; 🙂
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides                  ; highlighted indent columns
@@ -74,8 +74,9 @@
        (treemacs
         +lsp
         )                 ; a project drawer, like neotree but cooler
-       unicode                  ; extended unicode support for various languages
-       vc-gutter                ; vcs diff in the fringe
+       ;; unicode                  ; extended unicode support for various languages
+       ;; vc-gutter
+                                        ; vcs diff in the fringe
        vi-tilde-fringe          ; fringe tildes to mark beyond EOB
        (window-select
         +numbers
@@ -208,7 +209,7 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
