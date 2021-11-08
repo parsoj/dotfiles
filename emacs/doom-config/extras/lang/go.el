92a6535f5c04a569049d4aa61066975f5103f1b1
Jeff Parsons <parsoj@gmail.com>
2021-11-08 13:22:49 -0800

    force emacs to use lsp only for lookups

---
+++ emacs/doom-config/extras/lang/go.el
@@ -0,0 +1,6 @@
+;;; ../.config/emacs/doom-config/extras/lang/go.el -*- lexical-binding: t; -*-
+
+(set-lookup-handlers! 'go-mode
+  :definition #'lsp-ui-peek-find-definitions
+  :references #'lsp-ui-peek-find-references
+  )
