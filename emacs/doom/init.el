c86766cd3a164d8812578c5cd79d242ec1b289f0
Jeff Parsons <jeff@MacBook-Pro.local>
2021-12-10 08:44:32 -0800

    extra settings for ligatures support

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -61,7 +61,10 @@
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides                  ; highlighted indent columns
-       ligatures
+       (ligatures
+        +extra
+        +fira
+        )
        minimap                        ; show a map of the code on the side
        modeline                     ; snazzy, Atom-inspired modeline, plus API
        nav-flash                      ; blink cursor line after big motions
