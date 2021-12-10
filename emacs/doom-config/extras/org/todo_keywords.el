31d7cec7054bced1835135c908b9cc01ed857296
Jeff Parsons <jeff@MacBook-Pro.local>
2021-12-09 19:49:05 -0800

    rough initial todo keywords config

--- emacs/doom-config/extras/org/todo_keywords.el
+++ emacs/doom-config/extras/org/todo_keywords.el
@@ -8,3 +8,58 @@
     (add-file-local-variable 'org-todo-keyword-faces org-todo-keyword-faces)
     )
   )
+
+(let* (
+       (comment      "#6272a4")
+       (warning      "#ffb86c")
+       (rainbow-1    "#f8f8f2")
+       (rainbow-2    "#8be9fd")
+       (rainbow-3    "#bd93f9")
+       (rainbow-4    "#ff79c6")
+       (rainbow-5    "#ffb86c")
+       (rainbow-6    "#50fa7b")
+       (rainbow-7    "#f1fa8c")
+       (rainbow-8    "#0189cc")
+       (rainbow-9    "#ff5555")
+       (rainbow-10   "#a0522d")
+
+       ;; (variable-pitch-font `(:family "iA Writer Quattro S" ))
+       ;; (fixed-pitch-font    `(:family "Fira Mono" ))
+       ;; (fixed-pitch-font-alt `(:family "iA Writer Mono S" ))
+       )
+
+  (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)")
+                           (sequence "WAIT(w)" "LOWPRIO(l)")))
+
+  ;; (setq org-todo-keyword-faces (list
+  ;;                               `("TODO"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,comment
+  ;;                                 :weight bold
+  ;;                                 )
+  ;;                               `("NEXT"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,warning
+  ;;                                 :weight bold)
+  ;;                               `("WAIT"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,rainbow-2
+  ;;                                 :weight bold)
+  ;;                               `("VERIFY"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,rainbow-7
+  ;;                                 :weight bold)
+  ;;                               `("LOWPRIO"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,comment
+  ;;                                 :weight bold)
+  ;;                               `("DONE"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,rainbow-6
+  ;;                                 :weight bold)
+  ;;                               `("CANCELLED"
+  ;;                                 ,@fixed-pitch-font
+  ;;                                 :foreground ,rainbow-9
+  ;;                                 :weight bold)
+  ;;                               ))
+  )
