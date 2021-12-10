5c77ad437b181c6da0c2da39c9745ed2520e2316
Jeff Parsons <parsoj@gmail.com>
2021-12-10 14:27:34 -0800

    fix fresh-install script

--- emacs/fresh-install.sh
+++ emacs/fresh-install.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 set -e
 
 brew tap railwaycat/emacsmacport
+brew uninstall emacs-mac
 brew install emacs-mac --with-modules --with-no-title-bars
 ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
 
-./link.sh
+./link-configs.sh
+./install-fonts.sh
 
 rm -rf ~/.emacs.d
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
