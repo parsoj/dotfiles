44a1152c10142630e2cbe2d5cfbb7e1a91c918f5
Jeff Parsons <parsoj@gmail.com>
2022-01-25 09:50:49 -0800

    update safe vars

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -5,9 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
- '(package-selected-packages '(string-inflection))
+ '(package-selected-packages '(focus string-inflection))
  '(safe-local-variable-values
-   '((org-todo-keywords-1
+   '((projectile-project-run-cmd . "make test")
+     (nil . "make test")
+     (org-todo-keywords-1
       (sequence "TODO" "|" "DONE" "CANCELLED"))
      (org-todo-keyword-faces
       ("TODO" . "orange")
@@ -21,4 +23,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-peek-header ((t (:background "grey50" :foreground "white"))))
+ '(lsp-ui-peek-highlight ((t (:background "goldenrod" :foreground "#282c34"))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
