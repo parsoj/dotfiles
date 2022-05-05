93ab6e9dcdd9fcec384dd85a4d474975b4a92c25
Jeff Parsons <jeff@jeffparsons.me>
2022-05-04 17:09:56 -0700

    stick with doom-one themes for now

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -42,8 +42,8 @@
   "Load theme, taking current system APPEARANCE into consideration."
   (mapc #'disable-theme custom-enabled-themes)
   (pcase appearance
-    ('light (load-theme 'doom-tomorrow-day t))
-    ('dark (load-theme 'doom-palenight t))))
+    ('light (load-theme 'doom-one-light t))
+    ('dark (load-theme 'doom-one t))))
 
 (add-hook 'ns-system-appearance-change-functions #'sync-osx-dark-mode)
 
