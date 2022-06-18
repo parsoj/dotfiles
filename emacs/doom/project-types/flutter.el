bfd96f59b7fe1299102c2937ae358c431cd28567
Jeff Parsons <Jeff.Parsons@dreambox.com>
2022-06-18 09:05:32 -0700

    wrap flutter hot-reload func

--- emacs/doom/project-types/flutter.el
+++ emacs/doom/project-types/flutter.el
@@ -3,6 +3,22 @@
 
 
 ;;; trigger the hot-reloading in the flutter vterm
-(with-current-buffer "*vterm*"
-  (let ((inhibit-read-only t))
-    (vterm-send-string "r")))
+;; (with-current-buffer "*vterm*"
+;;   (let ((inhibit-read-only t))
+;;     (vterm-send-string "r")))
+
+
+(defun jp/flutter-run-or-hot-reload ()
+  (interactive)
+  (progn
+    (pop-to-buffer "*Flutter*")
+    (flutter-run-or-hot-reload)
+    )
+  )
+
+
+(map! :leader
+      :mode 'flutter
+      :desc "run-or-hot-reload" "m r" #'jp/flutter-run-or-hot-reload
+
+      )
