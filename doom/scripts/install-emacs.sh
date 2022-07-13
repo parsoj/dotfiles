0e941ee1d17f4c74adc1717056f37d3580780fcd
Jeff Parsons <Jeff.Parsons@dreambox.com>
2022-07-12 18:19:14 -0700

    add to the emacs installer script

--- doom/scripts/install-emacs.sh
+++ doom/scripts/install-emacs.sh
@@ -1,4 +1,45 @@
 #!/usr/bin/env bash
 
-brew tap railwaycat/emacsmacport
-brew install emacs-mac --with-native-comp --with-xwidgets --with-no-title-bars
+
+install_emacs_mac() {
+    echo "\ninstalling emacs-mac..."
+    if brew list emacs-mac &>/dev/null; then
+        echo "emacs-mac is already installed"
+    else
+        brew tap railwaycat/emacsmacport
+        brew install emacs-mac --with-native-comp --with-xwidgets --with-natural-title-bar --with-emacs-big-sur-icon \
+            && echo "emacs-mac is installed"
+    fi
+}
+
+install_emacs_mac
+
+
+clone_doom() {
+    echo "\ncloning doom emacs..."
+    if [ -d "~/.emacs.d" ]
+    then
+        echo "doom emacs is already cloned."
+    else
+        git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+    fi
+
+}
+
+clone_doom
+
+
+echo "running doom-clean"
+doom clean
+echo "done."
+
+echo "syncing doom config"
+doom sync
+echo "done."
+
+echo "generating doom env"
+doom env
+echo "done."
+
+echo "installing all-the-icons"
+emacs --batch -f all-the-icons-install-fonts
