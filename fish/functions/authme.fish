5448de53147ed1471faaf0b478ce6ec16f023f65
Jeff Parsons <Jeff.Parsons@dreambox.com>
2024-03-15 00:20:33 -0400

    store vault tokens in a stash file

--- fish/functions/authme.fish
+++ fish/functions/authme.fish
@@ -1,8 +1,26 @@
 function authme
-    echo "Checking vault token..."
+    if not set -q VAULT_ADDR
+        echo "VAULT_ADDR environment variable is not set. Please set it and try again."
+        return 1
+    end
+
+    set VAULT_TOKEN (grep -w $VAULT_ADDR ~/.vault-tokens-stash | awk '{print $2}' | tail -n 1)
+    
+    if set -q VAULT_TOKEN
+        echo "Found token in stash for the current VAULT_ADDR."
+    end
+    
     if not vault token lookup > /dev/null
-        echo "Invalid or expired Vault token. Logging in again."
-        vault login -method=okta username=Jeff.Parsons
+        echo "Valid token not found - Logging in again."
+        set NEW_VAULT_TOKEN (vault login -method=okta username=Jeff.Parsons -format=json | jq -r .auth.client_token)
+        set -gx VAULT_TOKEN $NEW_VAULT_TOKEN
+
+        # Inform the user that we're writing the new token to the stash
+        echo "Writing new token to stash."
+
+        # Update entry in .vault-tokens-stash
+        sed -i '' "/^$VAULT_ADDR/d" ~/.vault-tokens-stash
+        echo "$VAULT_ADDR $NEW_VAULT_TOKEN" >> ~/.vault-tokens-stash
     else
         echo "Current Vault token is valid."
     end
