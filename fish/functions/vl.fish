389b47553de8ed4adced31a1f1ca5880763da147
Jeff_Parsons <JParsons@discoveryed.com>
2024-06-24 17:11:07 -0400

    update/fix vl.fish script

--- fish/functions/vl.fish
+++ fish/functions/vl.fish
@@ -26,9 +26,21 @@ set -U all_environments '
 }
 '
 
+
+
+function check_vault_token
+    vault token lookup >/dev/null 2>&1
+    return $status
+end
+
+function vault_login
+    vault login -field=token -method=okta username=Jeff.Parsons
+    # return $login_output
+end
+
 function vl
     if test (count $argv) -ne 1
-        echo "Error: cl function only accepts one argument (the environment name)"
+        echo "Error: vl function only accepts one argument (the environment name)"
         return 1
     end
 
@@ -43,40 +55,61 @@ function vl
     # Parse the environment's properties from the JSON data
     set -lx environment_properties (echo $all_environments | jq -r ".$environment_name | to_entries | .[] | .key + \" \" + .value")
 
-    for property in $environment_properties
-        set -lx key (echo $property | cut -d' ' -f1)
-        set -lx value (echo $property | cut -d' ' -f2-)
+    set -lx property $environment_properties[1]
+    set -lx key (echo $property | cut -d' ' -f1)
+    set -lx value (echo $property | cut -d' ' -f2-)
 
-        if test -z "$value"
-            echo "Could not find property $key for environment $environment_name"
-            return 1
-        end
-
-        set -gx $key $value
+    echo "looking up vault url for '$key'"
+    if test -z "$value"
+        echo "Could not find property $key for environment $environment_name"
+        return 1
     end
+
+    echo "found vault url: $value"
+
+    set -gx $key $value
 
     # authme # Call to authme function
 
     # vault login -method=okta username=Jeff.Parsons
+    echo "first, lets unset VAULT_TOKEN..."
+    set -e VAULT_TOKEN
 
     # Call the vault-token-helper and capture the output
+    echo "checking cache for vault token..."
     set token (vault-token-helper get)
 
     # Check if the token is empty
     if test -z "$token"
+        echo "token not found. logging in"
         # If the token is empty, call vault login with the specified method and username
-        vault login -method=okta username=Jeff.Parsons
+        set -Ux VAULT_TOKEN (vault_login)
+    else
+        echo "token found in cache"
+        set -Ux VAULT_TOKEN "$token"
+
+        echo "checking if token is still valid..."
+        check_vault_token
+        if test $status -ne 0
+            echo "vault token is no longer valid. Logging in...."
+            set -Ux VAULT_TOKEN (vault_login)
+        else
+            echo "token is still valid."
+
+            echo "Done."
+        end
     end
 
-
 end
+
+
 
 function _vl_complete
     set -lx environments $all_environments
     set -lx environment_names (echo $environments | jq -r 'keys | .[]')
     for environment_name in $environment_names
-        #complete -c cl -f -a "$environment_name"
-        #complete -c cl -n 'count (commandline -opc) < 2' -f -a "$environment_name"
+        #complete -c vl -f -a "$environment_name"
+        #complete -c vl -n 'count (commandline -opc) < 2' -f -a "$environment_name"
         complete -c vl --no-files -n "not __fish_seen_subcommand_from $environment_names" -f -a "$environment_name"
     end
     complete -c vl --no-files
