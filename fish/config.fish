22624b8e4f155a3c4b89d9cb295b714a3352becd
Jeff Parsons <jp@jeffparsons.me>
2024-08-11 08:59:47 -0400

    alias for workspace func

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,10 @@
 # Commands to run in interactive sessions can go here
 #end
 
+alias refresh_config="source ~/.config/fish/config.fish"
+
 fish_add_path /opt/homebrew/bin
 fish_add_path (brew --prefix python)/libexec/bin
-
 
 # DBL stuff
 
@@ -15,16 +16,37 @@ fish_add_path (brew --prefix python)/libexec/bin
 # end
 
 #oh-my-posh --init --shell fish --config ~/.poshthemes/cloud-native.json | source
+#
+
+fish_vi_key_bindings
+
 
 alias tg=terragrunt
 alias tf=terraform
 alias vssh="vault-ssh connect"
 
+alias nv=nvim
+
 #source /Users/Jeff.Parsons/.docker/init-fish.sh || true # Added by Docker Desktop
 direnv hook fish | source
 STARSHIP_CONFIG=~/.config/starship/starship.toml starship init fish | source
-# starship init fish | source
+#starship init fish | source
 
+
+
+set -x PATH $HOME/.luaver/lua/5.1/bin $PATH
+set -x LUA_PATH "$HOME/.luaver/lua/5.1/share/lua/5.1/?.lua;$HOME/.luaver/lua/5.1/share/lua/5.1/?/init.lua;./?.lua;$HOME/.luaver/lua/5.1/lib/lua/5.1/?.lua;$HOME/.luaver/lua/5.1/lib/lua/5.1/?/init.lua"
+set -x LUA_CPATH "$HOME/.luaver/lua/5.1/lib/lua/5.1/?.so;./?.so"
+set -x LUAROCKS_CONFIG "$HOME/.luaver/lua/5.1/luarocks/config-5.1.lua"
+
+
+################################################################################
+# Workspaces settings
+
+# load workspaces functions
+set -U fish_function_path $fish_function_path ~/.config/fish/functions/workspaces
+
+alias ff=open_workspace_file
 
 
 
