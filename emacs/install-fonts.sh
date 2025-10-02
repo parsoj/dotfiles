f9fc74810f189c64f69f65840c66bd0695fb5c8d
Jeff Parsons <jeff@tennr.com>
2025-10-02 07:50:34 -0400

    remove emacs

--- emacs/install-fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code
-brew install --cask font-et-book
-brew install --cask font-eb-garamond
--- emacs/link-configs.sh
+++
@@ -1,4 +0,0 @@
-EMACS_CONF_ROOT=~/.config/emacs
-
-rm -rf ~/.doom.d
-ln -s $EMACS_CONF_ROOT/doom ~/.doom.d
